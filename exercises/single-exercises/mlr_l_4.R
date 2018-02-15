31bf58775a7e6b78668a3d68c22962ce759240d4
Christoph Molnar <christoph.molnar@gmail.com>
2018-02-15 11:27:59 +0100

    make solution easier

--- exercises/single-exercises/mlr_l_4.R
+++ exercises/single-exercises/mlr_l_4.R
@@ -12,25 +12,10 @@ mydata2 = as.data.frame(mydata1)
 
 task = makeClassifTask(data = mydata2, target = "classes")
 
-lrn = makeLearner("classif.kknn")
-
-myplot = function(learner, par.vals) {
-  for (j in seq_row(par.vals)) {
-    learner = setHyperPars(learner, par.vals = as.list(par.vals[j, , drop = FALSE]))
-    pl = plotLearnerPrediction(learner, task)
-    print(pl)
-    pause()
-  }
-}
-
-
 plotLearnerPrediction("classif.lda", task)
 plotLearnerPrediction("classif.qda", task)
-plotLearnerPrediction("classif.rpart", task)
-
-myplot(
-  learner = makeLearner("classif.kknn"),
-  par.vals = data.frame(k = c(1, 5, 50, 450))
-)
-
+plotLearnerPrediction("classif.logreg", task)
+plotLearnerPrediction("classif.kknn", task, k = 5)
+plotLearnerPrediction("classif.kknn", task, k = 1)
+plotLearnerPrediction("classif.kknn", task, k = 100)
 
