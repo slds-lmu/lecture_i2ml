113dca2a003ddb1a27ebae5a365250a8ba96f6be
Goschjann <jann.goschenhofer@posteo.de>
2018-02-18 18:21:18 +0100

    fix #168

--- code-demos/help4.R
+++ code-demos/help4.R
@@ -84,6 +84,31 @@ plotParamSequenceRPart(task = task, param = "cp", values = cps, minsplit = 5)
 
 
 
+
+################
+#
+#  Invariance of features
+#
+#############
+
+trainlog = train1 %>% mutate_at(c("Pgain", "Vgain"), log)
+head(trainlog, 3)
+head(train1, 3)
+
+model1 = rpart(Class ~., data = train1, minsplit = 30)
+rattle::fancyRpartPlot(model1, palettes = "Oranges", main = "CART with normal data", sub = "")
+
+
+modellog = rpart(Class ~., data = trainlog, minsplit = 30)
+rattle::fancyRpartPlot(modellog, palettes = "Blues", main = "CART with logged data", sub = "")
+
+
+
+
+
+
+
+
 #################
 #
 #   Random Forests and variable importance
@@ -100,6 +125,62 @@ var = as.data.frame(t(varimp$res))
 var$names = names(varimp$res)
 p = ggplot(data = var, aes(x = names, y = V1)) + geom_bar(stat = "identity")
 p
+
+
+
+### partykit
+library(partykit)
+
+modForest = cforest(Class ~ Motor + Screw + Pgain + Vgain, data = train1)
+var = as.data.frame(varimp(modForest))
+var$names = rownames(var)
+colnames(var) = c("varimp", "names")
+p = ggplot(data = var, aes(x = names, y = varimp)) +
+    geom_bar(stat = "identity") +
+    ggtitle(label = "Variable Importance from partykit")
+p
+
+##########################
+#
+#     Proximity
+#
+##########################
+
+
+library(randomForest)
+library(mlbench)
+
+# get data
+spiral = mlbench::mlbench.spirals(500, cycles = 1, sd = 0.1)
+p = ggplot(data = as.data.frame(spiral$x), aes(x = V1, y = V2, colour = spiral$classes)) +
+  geom_point()
+p
+
+# fit forest and extract proximity measure
+modProx = randomForest(x = spiral$x, y = spiral$classes, proximity = TRUE, oob.prox = TRUE)
+proxMat = modProx$proximity
+proxMat[1:5, 1:5]
+
+# apply mds on the proximity matrix
+mds = as.data.frame(cmdscale(proxMat))
+mds$class = spiral$classes
+
+# plot the result, sweet
+p = ggplot(data = mds, aes(x = V1, y = V2, colour = class)) +
+  geom_point() +
+  labs(x = "1st dimension", y = "2nd dimension", title = "Multidimensional Scaling of Spiral data based on proximity")
+p
+
+
+
+
+
+
+
+
+
+
+
 
 
 
