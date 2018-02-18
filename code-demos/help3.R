48073c4184322c99b64fff469889acaf14b24ff9
Goschjann <jann.goschenhofer@posteo.de>
2018-02-18 13:52:42 +0100

    fix 158 and change to spiral in cs3

--- code-demos/help3.R
+++ code-demos/help3.R
@@ -7,44 +7,62 @@
 ##############
 
 library(mlr)
+library(mlbench)
 
 # data
 data(iris)
 str(iris)
-set.seed(1327)
+set.seed(1333)
 trainSize = 3/4
 trainIndices = sample(x = seq(1, nrow(iris), by = 1), size = ceiling(trainSize * nrow(iris)), replace = FALSE)
 irisTrain = iris[ trainIndices, ]
 irisTest = iris[ -trainIndices, ]
 
+
+library(mlr)
+library(mlbench)
+
+set.seed(13)
+spiral = as.data.frame(mlbench.spirals(n = 500, sd = 0.1))
+trainSize = 3/4
+trainIndices = sample(x = seq(1, nrow(spiral), by = 1), size = ceiling(trainSize * nrow(spiral)), replace = FALSE)
+spiralTrain = spiral[ trainIndices, ]
+spiralTest = spiral[ -trainIndices, ]
+
+
+
 # run experiment
-k.values = rev(c(1, 2, 3, 4, 5, 7, 8, 9, 10, 15, 20, 25, 30, 35, 45, 50, 80, 100))
+k.values = rev(c(1, 2, 3, 4, 5, 7, 8, 9, 10, 15, 20, 25, 30, 35, 45, 50, 60, 70, 80, 90, 100))
 storage = data.frame(matrix(NA, ncol = 3, nrow = length(k.values)))
 colnames(storage) = c("mmce_train", "mmce_test", "k")
 
 for (i in 1:length(k.values)) {
 
-  irisTask = makeClassifTask(data = irisTrain, target = "Species")
-  irisLearner = makeLearner("classif.kknn", k = k.values[i])
-  irisModel = train(learner = irisLearner, task = irisTask)
+  spiralTask = makeClassifTask(data = spiralTrain, target = "classes")
+  spiralLearner = makeLearner("classif.kknn", k = k.values[i])
+  spiralModel = train(learner = spiralLearner, task = spiralTask)
 
   # test data
   # choose additional measures from: listMeasures(irisTask)
-  irisPred = predict(irisModel, newdata = irisTest[])
-  storage[i, "mmce_test"] = performance(pred = irisPred, measures = mmce)
+  spiralPred = predict(spiralModel, newdata = spiralTest[])
+  storage[i, "mmce_test"] = performance(pred = spiralPred, measures = mmce)
 
   # train data
-  irisPred = predict(irisModel, newdata = irisTrain[])
-  storage[i, "mmce_train"] = performance(pred = irisPred, measures = mmce)
+  spiralPred = predict(spiralModel, newdata = spiralTrain[])
+  storage[i, "mmce_train"] = performance(pred = spiralPred, measures = mmce)
 
   storage[i, "k"] = k.values[i]
 }
 
+storage = storage[rev(order(storage$k)),]
 
 plot(x = storage$k, y = storage$mmce_train, main = "Overfitting behavior KNN",
-  xlab = "k", ylab = "mmce", col = "blue", type = "l")
+  xlab = "k", ylab = "mmce", col = "blue", type = "l",
+  xlim = rev(range(storage$k)),
+  ylim = c(min(storage$mmce_train, storage$mmce_test),
+    max(storage$mmce_train, storage$mmce_test)))
 lines(x = storage$k, y = storage$mmce_test, col = "orange")
-legend("bottomright", c("test", "train"), col = c("orange", "blue"), lty = 1)
+legend("bottomleft", c("test", "train"), col = c("orange", "blue"), lty = 1)
 
 
 
@@ -60,7 +78,7 @@ legend("bottomright", c("test", "train"), col = c("orange", "blue"), lty = 1)
 
 selfCV = function(data, target, folds, k) {
 
-  storage = as.numeric(folds)
+  storage = numeric(folds)
 
   set.seed(1337)
   indices = sample(x = seq(1, nrow(data), by = 1), size = nrow(data), replace = FALSE)
@@ -72,20 +90,20 @@ selfCV = function(data, target, folds, k) {
 
     # data
     testData = data[indexMat[i, ], ]
-    trainData = data[- indexMat[i, ], ]
+    trainData = data[-indexMat[i, ], ]
 
     # model
-    irisTask = makeClassifTask(data = trainData, target = target)
-    irisLearner = makeLearner("classif.kknn", k = k)
-    irisModel = train(learner = irisLearner, task = irisTask)
-    storage[i] = performance(predict(irisModel, newdata = testData),
+    task = makeClassifTask(data = trainData, target = target)
+    learner = makeLearner("classif.kknn", k = k)
+    model = train(learner = learner, task = task)
+    storage[i] = performance(predict(model, newdata = testData),
       measures = mmce)[[1]]
   }
 
   return(list(folds = folds, storage = as.data.frame(storage), GE = mean(storage)))
 }
 
-foo = selfCV(data = iris, target = "Species", folds = 10, k = 3)
+foo = selfCV(data = spiral, target = "classes", folds = 10, k = 3)
 
 p = ggplot(data = foo$storage, aes(y = storage, x = 1)) +
   geom_boxplot() +
@@ -95,8 +113,8 @@ p
 
 
 
-set.seed(1337)
-task = mlr::makeClassifTask(data = iris, target = "Species")
+set.seed(1333)
+task = mlr::makeClassifTask(data = spiral, target = "classes")
 rdescCV = mlr::makeResampleDesc(method = "CV", iters = 10)
 mlrCV = resample(learner = "classif.knn", k = 4, task = task,
   resampling = rdescCV, show.info = FALSE)
@@ -110,119 +128,92 @@ p = ggplot(data = mlrCV$measures.test, aes(y = mmce, x = 1)) +
 p
 
 
-
-
-# repeat 10 times to show that holdout is more depending on the randomness
-storage = as.data.frame(matrix(0, nrow = 50, ncol = 2))
-colnames(storage) = c("10-fold CV", "Holdout")
-
-for (i in 1:nrow(storage)) {
-  rCv = mlr::resample(learner = "classif.knn", k = 4, task = task,
-    resampling = rdescCV, show.info = FALSE)
-  rHO = mlr::resample(learner = "classif.knn", k = 4, task = task,
-    resampling = rdescHO, show.info = FALSE)
-
-  storage[i, "10-fold CV"] = as.numeric(rCv$aggr)
-  storage[i, "Holdout"] = as.numeric(rHO$aggr)
-}
-
-
-
 #################
 #
 # Nested Resampling
 #
 ##############
 
-selfNR = function(data, target, outerFolds = 3, innerFolds = 4, kCandidates) {
+selfNR = function(data, target, outerFolds = 3, innerFolds = 4, kCandidates, inform = FALSE) {
 
-  data = iris
-  outerFolds = 3
-  innerFolds = 5
+  # data = spiral
+  # outerFolds = 4
+  # innerFolds = 5
+  counter = 0
 
   set.seed(1337)
   outerIndices = sample(x = seq(1, nrow(data), by = 1), size = nrow(data), replace = FALSE)
-  indexMat = matrix(data = outerIndices, byrow = TRUE, nrow = outerFolds)
+  indexOuterMat = matrix(data = outerIndices, byrow = TRUE, nrow = outerFolds)
+
+  # frame to store the winner and its test-GE from all outer folds
+  winnerCV = as.data.frame(matrix(0, nrow = outerFolds, ncol = 2))
+  colnames(winnerCV) = c("k", "GE")
 
   for (i in 1:outerFolds) {
 
     # split in validation and data for the inner loop
-    valData = data[outerIndices[i, ]]
-    innerData = data[ -outerIndices[i, ]]
+    valData = data[indexOuterMat[i, ], ]
+    innerData = data[ -indexOuterMat[i, ], ]
 
-    winnerCV = as.data.frame(matrix(0, nrow = innerFolds, ncol = 2))
+    # frame to store the CV-GEs for each candidate
+    candidateGE = as.data.frame(matrix(0, nrow = length(kCandidates), ncol = 2))
+    colnames(candidateGE) = c("k", "GE")
 
     # calculate GE for each of the candidates via CV
     for (l in 1:length(kCandidates)) {
 
-      candidateGE = as.data.frame(matrix(0, nrow = kCandidates, ncol = 2))
-      colnames(candidateGE) = c("k", "GE")
-
       innerIndices = sample(x = seq(1, nrow(innerData), by = 1), size = nrow(innerData), replace = FALSE)
 
       # index matrix for inner folds
-      indexInnerMat = matrix(data = indices, byrow = TRUE, nrow = folds)
+      indexInnerMat = matrix(data = innerIndices, byrow = TRUE, nrow = innerFolds)
 
       # storage for CV errors for one candidate
-      storageCV = as.numeric(innerFolds)
+      storageInnerCV = numeric(innerFolds)
 
       for (j in 1:innerFolds) {
           # data
-          testData = data[indexMat[i, ], ]
-          trainData = data[- indexMat[i, ], ]
+          testData = innerData[indexInnerMat[j, ], ]
+          trainData = innerData[ -indexInnerMat[j, ], ]
 
           # model
-          irisTask = makeClassifTask(data = trainData, target = target)
-          irisLearner = makeLearner("classif.kknn", k = kCandidates[l])
-          irisModel = train(learner = irisLearner, task = irisTask)
-          storageCV[i] = performance(predict(irisModel, newdata = testData),
+          task = makeClassifTask(data = trainData, target = target)
+          learner = makeLearner("classif.kknn", k = kCandidates[l])
+          model = train(learner = learner, task = task)
+          # inform user about progress
+          counter = counter + 1
+          if (inform) print(paste0("model: " ,counter, " inner fold: ", j, " outer fold: ", i))
+          storageInnerCV[j] = performance(predict(model, newdata = testData),
             measures = mmce)[[1]]
       }
 
       # CV GE for candidate l
-      candidateGE[l, "GE"] = mean(storageCV)
+      candidateGE[l, "GE"] = mean(storageInnerCV)
+      candidateGE[l, "k"] = kCandidates[l]
     }
 
-  }
+  # get GE for best candidate in this outerFold
+  bestCandidate = candidateGE[which(candidateGE$GE == min(candidateGE$GE)), "k"]
+  winnerCV[i, "k"] = bestCandidate
 
-  innerIndices =
+
+  # model
+  task = makeClassifTask(data = innerData, target = target)
+  learner = makeLearner("classif.kknn", k = kCandidates[l])
+  model = train(learner = learner, task = task)
+  winnerCV[i, "GE"] = performance(predict(model, newdata = valData),
+    measures = mmce)[[1]]
+  }
+  return(winnerCV)
 }
 
+foo = selfNR(data = spiral, target = "classes", outerFolds = 10, innerFolds = 9,
+  kCandidates = c(20, 3, 5, 10, 1), inform = TRUE)
+foo
 
-
-
-selfCV = function(data, target, folds, k) {
-
-  storage = as.numeric(folds)
-
-  set.seed(1337)
-  indices = sample(x = seq(1, nrow(data), by = 1), size = nrow(data), replace = FALSE)
-
-  # index matrix for folds
-  indexMat = matrix(data = indices, byrow = TRUE, nrow = folds)
-
-  for (i in 1:folds) {
-
-    # data
-    testData = data[indexMat[i, ], ]
-    trainData = data[- indexMat[i, ], ]
-
-    # model
-    irisTask = makeClassifTask(data = trainData, target = target)
-    irisLearner = makeLearner("classif.kknn", k = k)
-    irisModel = train(learner = irisLearner, task = irisTask)
-    storage[i] = performance(predict(irisModel, newdata = testData),
-      measures = mmce)[[1]]
-  }
-
-  return(list(folds = folds, storage = as.data.frame(storage), GE = mean(storage)))
-}
-
-
-
-
-
-
+debug(selfNR)
+foo = selfNR(data = spiral, target = "classes", outerFolds = 4, innerFolds = 5,
+  kCandidates = c(20, 3, 5, 10, 1))
+undebug(selfNR)
 
 
 
