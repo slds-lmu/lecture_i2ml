ac352690014cbd0ef7128bedf164b296bce0281d
Fabian Scheipl <fabian.scheipl@googlemail.com>
2019-11-15 19:02:34 +0100

    [WIP] avoiding duplicate short names

--- attic/slide_dependencies.R
+++ attic/slide_dependencies.R
@@ -3,60 +3,78 @@ library("igraph")
 library("plyr")
 
 ## Get all slide Rnws
-slidedir <- "../slides/"
-rnws <- list.files(path = slidedir, pattern = "*.Rnw", recursive = TRUE)
-nams <- gsub(pattern = "*/slides|.Rnw", replacement = "", x = rnws)
+slidedir <- "./slides/"
+topics <- fs::dir_ls(path = slidedir, type = "directory")
+slides <- purrr::map(topics, 
+                     ~ list.files(path = .x, pattern = "*.Rnw", full.names = TRUE))
 
-## Read all lines
-all_txt <- lapply(paste0(slidedir, rnws), readLines)
-names(all_txt) <- nams
-
-## Find relevant lines
-includes <- lapply(all_txt, function(x) grep("%! includes:*", x, value = TRUE))
-
-## get relevant words
-get_dependencies <- function(nam) {
-    
-    x <- includes[[nam]]
-    
-    ## remove %! includes:
-    x <- gsub(pattern = "%! includes: ", replacement = "", x)
-    
-    ## split into single strings
-    if (length(x) == 0) {
-        dp <- NA 
-    } else {
-        dp <- unlist(strsplit(x = x, split = ", "))
-        
-        ## remove spaces
-        dp <- gsub(pattern = " ", replacement = "", dp)
-        
-        ## get correct name
-        dp <- sapply(dp, grep, x = nams, value = TRUE, USE.NAMES = FALSE)
+## get includes
+get_dependencies <- function(slideset) {
+    inc <- grep("%! includes:*", readLines(slideset), value = TRUE) 
+    if (length(inc)) {
+        slides <- gsub(pattern = "^slides-(.+)\\.Rnw$", 
+                       replacement = "\\1", 
+                       basename(slideset))
+        data.frame(
+            slides = slides,
+            includes = included_dependencies(inc),
+            stringsAsFactors = FALSE
+        )
     }
-    
-    data.frame(target = nam, depends = unlist(dp), stringsAsFactors = FALSE)
 }
 
-depends <- ldply(names(includes), get_dependencies)
-dependencymat <- na.omit(depends)
-
-dependencymat_simple <- dependencymat
-
-simplify_dp <- function(x) {
-    nams <- gsub(".*([^-]*-[^-]*)-*$", "\\1", x)
-    gsub("-", "", nams)
+## get dependencies from include statement
+included_dependencies <- function(inc) {
+    inc <- gsub(pattern = "%! includes: ", replacement = "", inc)
+    deps <- unlist(strsplit(x = inc, split = ", "))
+    ## split into single strings
+    if (length(deps) == 0) {
+        return(NA)
+    }
+    gsub(pattern = " ", replacement = "", deps)
 }
 
-dependencymat_simple$target <- simplify_dp(dependencymat_simple$target)
-dependencymat_simple$depends <- simplify_dp(dependencymat_simple$depends)
+
+includes <- purrr::map_depth(slides, 2, get_dependencies) %>% 
+    purrr::map(~ purrr::discard(., is.null)) %>% 
+    purrr::map(~ do.call(rbind, .)) %>% 
+    purrr::discard(., is.null) %>% 
+    purrr::imap(~ cbind(.x, topic = basename(.y), 
+                        stringsAsFactors = FALSE)) %>% 
+    { 
+        do.call(rbind, .) 
+    }
+
+includes$slides_short <- gsub("^[a-z]+-", "", includes$slides)
+includes$includes_short <- gsub("^[a-z]+-", "", includes$includes)   
+with(includes) {
+    for (slide in unique(slides)) {
+        slide_short <- slides_short[[]] # ?!?!?!
+    }
+}
 
 
-## Create a dependency matrix (using igraph package)
-graph_simple <- graph.data.frame(dependencymat_simple)
-graph <- graph.data.frame(dependencymat)
-
-## ... and plot
-plot(graph, vertex.shape = "none", edge.arrow.size = 0.5)
-plot(graph_simple, vertex.shape = "none", edge.arrow.size = 0.5)
-
+# 
+# depends <- ldply(names(includes), get_dependencies)
+# dependencymat <- na.omit(depends)
+# 
+# dependencymat_simple <- dependencymat
+# 
+# simplify_dp <- function(x) {
+#     nams <- gsub(".*([^-]*-[^-]*)-*$", "\\1", x)
+#     gsub("-", "", nams)
+# }
+# 
+# dependencymat_simple$target <- simplify_dp(dependencymat_simple$target)
+# dependencymat_simple$depends <- simplify_dp(dependencymat_simple$depends)
+# 
+# 
+# ## Create a dependency matrix (using igraph package)
+# graph_simple <- graph.data.frame(dependencymat_simple)
+# graph <- graph.data.frame(dependencymat)
+# 
+# ## ... and plot
+# plot(graph, vertex.shape = "none", edge.arrow.size = 0.1)
+# plot(graph_simple, vertex.shape = "none", edge.arrow.size = 0.1)
+# plot(graph_simple, vertex.shape = "none", edge.arrow.size = 0.1, 
+#      layout = layout_with_sugiyama(graph_simple)$)
