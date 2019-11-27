0a4b0b760204a5c19ec5bb23d3a25a2172fb949b
Fabian Scheipl <fabian.scheipl@googlemail.com>
2019-11-27 22:52:14 +0100

    tuning overhaul (almost) done.
    some figures still need updates, see #415, #414, #398

--- slides/tuning/rsrc/tune_example.R
+++ slides/tuning/rsrc/tune_example.R
@@ -1,20 +1,24 @@
 library(mlr)
-set.seed(123)
+set.seed(12322)
 
-# just run RS on spam for gbm so we can show an example path
+library(parallelMap)
+parallelStartMulticore(15)
+
+# just run RS on spam for ranger so we can show an example path
 # that improves the result
 
-task = spam.task
-lrn = makeLearner("classif.gbm", predict.type="prob", distribution = "bernoulli")
+task = sonar.task
+learner = makeLearner("classif.ranger", predict.type = "prob")
 
-ps = makeParamSet(
-  makeIntegerParam("n.trees", lower = 3, upper = 500),
-  makeNumericParam("shrinkage", lower = 0, upper = 1),
-  makeIntegerParam("interaction.depth", lower = 1, upper = 5),
-  makeNumericParam("bag.fraction", lower = 0.2, upper = 0.9)
+params = makeParamSet(
+  makeIntegerParam("num.trees", lower = 3, upper = 200),
+  makeIntegerParam("mtry", lower = 5, upper = 20),
+  makeIntegerParam("min.node.size", lower = 2, upper = 20)
 )
-ctrl = makeTuneControlRandom(maxit = 50L)
-tr = tuneParams(lrn, task = sonar.task, par.set = ps, resampling = cv5,
+ctrl = makeTuneControlRandom(maxit = 150L)
+tuning = tuneParams(learner, task = task, par.set = params, resampling = cv5,
   control = ctrl, measures = mlr::auc)
 
-save2("tune_example.RData", tune_example_res = tr)
+BBmisc::save2("tune_example.RData", tune_example_res = tuning)
+
+parallelStop()
