a67f6f1fc3fb070d36e394dc0c879bdfc8247202
Tobias Pielok <t.pielok@gmail.com>
2020-02-06 16:52:11 +0100

    clarify ex 2

--- exercises/day4/exercises/randomForest_l_2.R
+++ exercises/day4/exercises/randomForest_l_2.R
@@ -1,22 +1,30 @@
-library(mlr)
+library(mlr3)
+library(mlr3learners)
+library(mlr3viz)
 library(mlbench)
 
+data <- as.data.frame(mlbench.spirals(500, sd = 0.1))
+task <- TaskClassif$new(id = "spirals", backend = data, target = "classes")
+
+num_trees <- c(1, 10, 20, 100, 1000)
+
 set.seed(123)
-data = mlbench.spirals(500, sd = 0.1)
-data = as.data.frame(data)
-task = makeClassifTask(data = data, target = "classes")
-
-
-mds = 1:5
-
-for (md in mds) {
-  print(plotLearnerPrediction("classif.rpart", task, maxdepth = md))
+nt <- 5000
+for (nt in num_trees) {
+  print(plot_learner_prediction(
+    learner = lrn("classif.ranger", num.trees = nt), 
+    task = task))
   BBmisc::pause()
 }
 
-ntrees = c(1, 10, 20, 100, 1000)
+plot_learner_prediction(
+  learner = lrn("classif.ranger", num.trees = nt), 
+  task = task)
 
-for (nt in ntrees) {
-  print(plotLearnerPrediction("classif.randomForest", task, ntree = nt))
-  BBmisc::pause()
-}
+# For a single tree, one observes a very non-smooth decision boundary 
+# as one would expect. The decision boundary becomes smoother with an 
+# increasing number of trees since bagging reduces the variance of the 
+# single classifier. Increasing the number of trees, when the number is 
+# already relatively high, does not affect the boundaries much, since the 
+# random forest has already converged.
+
