ec1bc31cd753061c6ebe3187eaac8000406bd97e
HeidiSeibold <hseibold@ibe.med.uni-muenchen.de>
2020-04-14 16:36:27 +0200

    get new cheatsheets to website

--- vignettes/get_cheatsheet_data.R
+++ vignettes/get_cheatsheet_data.R
@@ -8,10 +8,18 @@ cheat <- list.files(path = cdir, pattern = "CheatSheet_i2ml_[[:digit:]]*.pdf",
 ## Topics
 topics <- c("Notation, Definitions, Terms", 
     "Supervised classification",
-    "Evaluation")
+    "Supervised regression",
+    "Evaluation",
+    "trees")
 cdat <- data.frame(Topic = topics)
 
 
 ## Links
+# current fix, please update once
+# https://github.com/compstat-lmu/lecture_i2ml/issues/520
+# is fixed
+cheat <- cheat[c(1, 2, 5, 3, 4)]
+
+
 linkstring <- "https://nbviewer.jupyter.org/github/compstat-lmu/lecture_i2ml/blob/master/cheatsheets/"
 cdat$PDF <- paste0("[pdf](", linkstring, cheat, ")")
