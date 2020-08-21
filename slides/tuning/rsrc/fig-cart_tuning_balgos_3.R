b192a11f7654f7132e5c91b7a0232cd472421a3a
Tobias Pielok <t.pielok@gmail.com>
2020-08-21 18:53:58 +0200

    do not simul for every rendering (2nd)

--- slides/tuning/rsrc/fig-cart_tuning_balgos_3.R
+++ slides/tuning/rsrc/fig-cart_tuning_balgos_3.R
@@ -37,30 +37,6 @@ scale_c_d <- scale_colour_discrete <- scale_color_discrete <-
   }
 
 
-library(parallelMap)
-library(ranger)
-parallelStartMulticore(15)
-
-# just run RS on spam for ranger so we can show an example path
-# that improves the result
-
-task = sonar.task
-learner = makeLearner("classif.ranger", predict.type = "prob")
-
-params = makeParamSet(
-  makeIntegerParam("num.trees", lower = 3, upper = 200),
-  makeIntegerParam("mtry", lower = 5, upper = 20),
-  makeIntegerParam("min.node.size", lower = 2, upper = 20)
-)
-ctrl = makeTuneControlRandom(maxit = 150L)
-tuning = tuneParams(learner, task = task, par.set = params, resampling = cv5,
-                    control = ctrl, measures = mlr::auc)
-
-BBmisc::save2("tune_example.RData", tune_example_res = tuning)
-
-parallelStop()
-
-
 set.seed(12322)
 pdf("../figure/cart_tuning_balgos_3.pdf", width = 8, height = 3)
 tr = BBmisc::load2("tune_example.RData")
