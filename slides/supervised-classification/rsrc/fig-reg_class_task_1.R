ca9167c2a74223d1961022934ee9cde1d2e63ce0
Rifat M. Amin <amin.rifat@gmail.com>
2020-09-18 12:20:52 +0200

    Update fig-reg_class_task_1.R
    merged with fig-reg_class_task_2.R and fig-reg_class_task_3.R

--- slides/supervised-classification/rsrc/fig-reg_class_task_1.R
+++ slides/supervised-classification/rsrc/fig-reg_class_task_1.R
@@ -20,17 +20,29 @@ library(kknn)
 library(e1071)
 
 options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
-
+set.seed(123)
 
 plot_lp = function(...){
   plotLearnerPrediction(...) + scale_fill_viridis_d()
 }
 
-set.seed(600000)
-
+#logreg
 
 pdf("../figure/reg_class_task_1.pdf", width = 6, height = 6)
 plot_lp(makeLearner("classif.logreg"), sonar.task) + xlim(0,0.05) + ggtitle("Sonar")
 ggsave("../figure/reg_class_task_1.pdf", width = 6, height = 6)
 dev.off()
 
+#svm
+
+pdf("../figure/reg_class_task_2.pdf", width = 6, height = 6)
+plot_lp(makeLearner("classif.svm"), iris.task, c("Petal.Length", "Petal.Width")) + ggtitle("Iris") 
+ggsave("../figure/reg_class_task_2.pdf", width = 6, height = 6)
+dev.off()
+
+#
+library(GGally)
+pdf("../figure/reg_class_task_3.pdf", width = 8, height = 5)
+ggpairs(iris, mapping = aes(col = Species))
+ggsave("../figure/reg_class_task_3.pdf", width = 8, height = 5)
+dev.off()
