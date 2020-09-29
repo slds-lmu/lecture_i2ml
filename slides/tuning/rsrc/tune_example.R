39b2cc00bc2ea1016dc7b3fd04a54aad73899d40
Lisa Wimmer <wml1293@outlook.de>
2020-09-29 19:22:50 +0200

    replaced figures in forest-basicalogs

--- slides/tuning/rsrc/tune_example.R
+++ slides/tuning/rsrc/tune_example.R
@@ -1,23 +1,46 @@
-library(mlr)
-set.seed(12322)
+# PREREQ -----------------------------------------------------------------------
 
+library(mlr3)
+library(mlr3learners)
+library(mlr3tuning)
+library(paradox)
 library(parallelMap)
+
 parallelStartMulticore(15)
 
-# just run RS on spam for ranger so we can show an example path
+# DATA -------------------------------------------------------------------------
+
+set.seed(12322)
+
+# Just run RS on spam for ranger so we can show an example path
 # that improves the result
 
-task = sonar.task
-learner = makeLearner("classif.ranger", predict.type = "prob")
+task = tsk("sonar")
+learner = lrn("classif.ranger", predict_type = "prob")
+resampling = rsmp("cv", folds = 5)
+measure = msr("classif.auc")
 
-params = makeParamSet(
-  makeIntegerParam("num.trees", lower = 3, upper = 200),
-  makeIntegerParam("mtry", lower = 5, upper = 20),
-  makeIntegerParam("min.node.size", lower = 2, upper = 20)
+tune_ps = ParamSet$new(list(
+  ParamInt$new("num.trees", lower = 3, upper = 200),
+  ParamInt$new("mtry", lower = 5, upper = 20),
+  ParamInt$new("min.node.size", lower = 2, upper = 20)
+))
+
+terminator = trm("evals", n_evals = 150)
+
+instance = TuningInstanceSingleCrit$new(
+  task = task,
+  learner = learner,
+  resampling = resampling,
+  measure = measure,
+  search_space = tune_ps,
+  terminator = terminator,
+  store_models = TRUE
 )
-ctrl = makeTuneControlRandom(maxit = 150L)
-tuning = tuneParams(learner, task = task, par.set = params, resampling = cv5,
-  control = ctrl, measures = mlr::auc)
+
+tuner = tnr("random_search")
+tuner$optimize(instance)
+tuning = instance$archive
 
 BBmisc::save2("tune_example.RData", tune_example_res = tuning)
 
