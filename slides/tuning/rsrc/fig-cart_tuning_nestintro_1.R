583ed011f512dce0bbe48dd383da25f890fca479
Lisa Wimmer <wml1293@outlook.de>
2020-09-29 19:32:10 +0200

    replaced figurs in tuning-nestedintro

--- slides/tuning/rsrc/fig-cart_tuning_nestintro_1.R
+++ slides/tuning/rsrc/fig-cart_tuning_nestintro_1.R
@@ -1,54 +1,32 @@
- 
+# PREREQ -----------------------------------------------------------------------
+
 library(knitr)
-library(mlbench)
-library(mlr)
-library(OpenML)
-
 library(ggplot2)
-library(viridis)
-library(gridExtra)
-library(ggrepel)
-library(repr)
 
-library(data.table)
-library(BBmisc)
-
-library(party)
-library(rpart)
-library(rpart.plot)
-library(randomForest)
-library(rattle)
-library(smoof)
-library(kableExtra)
-library(kknn)
-library(e1071)
-library(rattle)
-library(reshape2)
-
-library(plyr)
-library(kernlab)
-
-options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
-
-
-scale_c_d <- scale_colour_discrete <- scale_color_discrete <-
-  function(...) {
-    viridis::scale_color_viridis(..., end = .9, discrete = TRUE, drop = TRUE)
-  }
-
-
-pdf("../figure/cart_tuning_nestintro_1.pdf", width = 8, height = 3.5)
+# DATA -------------------------------------------------------------------------
 
 ggd = BBmisc::load2("overtuning-example.RData", "res2.mean")
-ggd = reshape2::melt(ggd, measure.vars = colnames(res2), value.name = "tuneperf");
+
+ggd = reshape2::melt(ggd, 
+                     measure.vars = colnames(res2), 
+                     value.name = "tuneperf")
+
 colnames(ggd)[1:2] = c("data.size", "iter")
 ggd = BBmisc::sortByCol(ggd, c("data.size", "iter"))
 ggd$data.size = as.factor(ggd$data.size)
 
+# PLOT -------------------------------------------------------------------------
+
+pdf("../figure/cart_tuning_nestintro_1.pdf", width = 8, height = 3.5)
 
 pl = ggplot(ggd, aes(x = iter, y = tuneperf, col = data.size))
-pl =  pl + geom_line() + ylab("Tuning Error") + xlab("Iteration") + labs(colour = "Data Size")
+pl =  pl + 
+  geom_line() + 
+  ylab("Tuning Error") + 
+  xlab("Iteration") + 
+  labs(colour = "Data Size") +
+  scale_color_viridis_d(end = .9)
 print(pl)
+
 ggsave("../figure/cart_tuning_nestintro_1.pdf", width = 8, height = 3.5)
 dev.off()
-
--- slides/tuning/rsrc/fig-cart_tuning_nestintro_2.R
+++ slides/tuning/rsrc/fig-cart_tuning_nestintro_2.R
@@ -1,77 +1,66 @@
- 
+# PREREQ -----------------------------------------------------------------------
+
 library(knitr)
-library(mlbench)
-library(mlr)
-library(OpenML)
-
 library(ggplot2)
-library(viridis)
 library(gridExtra)
-library(ggrepel)
-library(repr)
 
-library(data.table)
-library(BBmisc)
+# FUNCTIONS --------------------------------------------------------------------
 
-library(party)
-library(rpart)
-library(rpart.plot)
-library(randomForest)
-library(rattle)
-library(smoof)
-library(kableExtra)
-library(kknn)
-library(e1071)
-library(rattle)
-library(reshape2)
+myd = function(x, size, p) dbinom(round(x * size), p = p, size = size)
 
-library(plyr)
-library(kernlab)
+sample_some_errs = function(k) rbinom(k, size = size, p = p) / size
 
-options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
+plot_dens_with_errs = function(k) {
+  
+  minerr = min(d.errs[1:k, "x"])
+  label = sprintf("n = %i; #runs = %i; best err = %.2f", size, k, minerr)
+  
+  pl = ggplot(mapping = aes(x = x, y = y)) + geom_line(data = d.dens)
+  pl = pl + geom_point(data = d.errs[1:k,])
+  pl = pl + geom_vline(xintercept = minerr, col = "red")
+  # pl = pl + geom_label()
+  pl = pl + ggtitle(label = label)
+  pl = pl + 
+    xlab("Test Error") + 
+    ylab("Density") + 
+    theme(plot.title = element_text(size = 12))
+  
+  return(pl)
+  
+}
 
+# DATA -------------------------------------------------------------------------
 
-scale_c_d <- scale_colour_discrete <- scale_color_discrete <-
-  function(...) {
-    viridis::scale_color_viridis(..., end = .9, discrete = TRUE, drop = TRUE)
-  }
+set.seed(123)
 
 size = 200
 p = 0.5
+
 dens.x = seq(0, 1, length.out = 100)
-myd = function(x, size, p) dbinom(round(x*size), p = p, size = size)
 dens.y = myd(dens.x, p = p, size = size)
 d.dens = data.frame(x = dens.x, y = dens.y)
-sample_some_errs = function(k) rbinom(k, size = size, p = p) / size
+
 errs = sample_some_errs(1000)
 d.errs = data.frame(x = errs, y = 0.0)
 
-plot_dens_with_errs = function(k) {
-  pl = ggplot(mapping = aes(x = x, y = y)) + geom_line(data = d.dens)
-  pl = pl + geom_point(data = d.errs[1:k,])
-  minerr = min(d.errs[1:k, "x"])
-  pl = pl + geom_vline(xintercept = minerr, col = "red")
-  # pl = pl + geom_label()
-  label = sprintf("n=%i; #runs=%i; best err=%.2f", size, k, minerr)
-  pl = pl + ggtitle(label = label)
-  pl = pl + xlab("Test Error") + ylab("Density") +  theme(plot.title = element_text(size = 12))
-  return(pl)
-}
+# PLOT 1 -----------------------------------------------------------------------
 
-#figure 1
 pdf("../figure/cart_tuning_nestintro_2.pdf", width = 8, height = 3.5)
+
 pl1 = plot_dens_with_errs(k = 1)
 pl2 = plot_dens_with_errs(k = 10)
 grid.arrange(pl1, pl2, ncol = 2)
+
 ggsave("../figure/cart_tuning_nestintro_2.pdf", width = 8, height = 3.5)
 dev.off()
 
+# PLOT 2 -----------------------------------------------------------------------
 
-#figure 2
 pdf("../figure/cart_tuning_nestintro_3.pdf", width = 8, height = 3.5)
+
 pl3 = plot_dens_with_errs(k = 100)
 pl4 = plot_dens_with_errs(k = 1000)
 grid.arrange(pl3, pl4, ncol = 2)
+
 ggsave("../figure/cart_tuning_nestintro_3.pdf", width = 8, height = 3.5)
 dev.off()
-
