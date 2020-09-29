2a96a146fdb4879f0aeb0697fc91d9f839c41989
Lisa Wimmer <wml1293@outlook.de>
2020-09-29 15:24:10 +0200

    replaced figures in evaluation-overfitting

--- slides/evaluation/rsrc/fig-eval_ofit_1.R
+++ slides/evaluation/rsrc/fig-eval_ofit_1.R
@@ -1,48 +1,52 @@
- 
+# PREREQ -----------------------------------------------------------------------
+
 library(knitr)
 library(mlbench)
-library(mlr)
-library(OpenML)
-
-library(ggplot2)
-library(viridis)
-library(gridExtra)
-library(ggrepel)
-library(repr)
-
-library(data.table)
-library(BBmisc)
-
-
-library(party)
-library(kableExtra)
-library(kknn)
-library(e1071)
-
-options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
-
-
-plot_lp = function(...){
-  plotLearnerPrediction(...) + scale_fill_viridis_d()
-}
-
 library(plyr)
+library(mlr3)
+library(mlr3learners)
+library(mlr3viz)
 library(kernlab)
+library(ggplot2)
+
+# DATA -------------------------------------------------------------------------
+
 set.seed(600000)
-pdf("../figure/eval_ofit_1.pdf", width = 8, height = 6)
 data = as.data.frame(mlbench.2dnormals(n = 200, cl = 3))
 data$classes = mapvalues(data$classes, "3", "1")
-task = makeClassifTask(data = data, target = "classes")
-learner = makeLearner("classif.ksvm")
-plotLearnerPrediction(learner, task, kernel = "rbfdot", C = 1, sigma = 100, pointsize = 4)
+
+task = TaskClassif$new("example", data, target = "classes")
+
+learner_of = lrn("classif.svm", 
+                 type = "C-classification", 
+                 kernel = "radial", 
+                 gamma = 100, 
+                 cost = 1,
+                 predict_type = "prob")
+
+learner_nof = lrn("classif.svm", 
+                  type = "C-classification", 
+                  kernel = "radial", 
+                  gamma = 1, 
+                  cost = 1,
+                  predict_type = "prob")
+
+# PLOT 1 -----------------------------------------------------------------------
+
+pdf("../figure/eval_ofit_1.pdf", width = 8, height = 6)
+
+plot_learner_prediction(learner_of, task) +
+  guides(shape = FALSE, alpha = FALSE)
 
 ggsave("../figure/eval_ofit_1.pdf", width = 8, height = 6)
 dev.off()
 
+# PLOT 2 -----------------------------------------------------------------------
 
 pdf("../figure/eval_ofit_2.pdf", width = 8, height = 6)
-plotLearnerPrediction(learner, task, kernel = "rbfdot", C = 1, sigma = 1, pointsize = 4)
+
+plot_learner_prediction(learner_nof, task) +
+  guides(shape = FALSE, alpha = FALSE)
 
 ggsave("../figure/eval_ofit_2.pdf", width = 8, height = 6)
 dev.off()
-
---
+++ slides/evaluation/rsrc/fig-eval_ofit_2.R
@@ -0,0 +1,39 @@
+# PREREQ -----------------------------------------------------------------------
+
+library(knitr)
+
+# DATA -------------------------------------------------------------------------
+
+set.seed(600000)
+
+x <- seq(0, 1, length.out = 20)
+y1 <- c(1 - x[1:4], 
+        1.5 - 4 * x[5:6], 
+        0.5 - 0.8 * x[7:10], 
+        0.15 - 0.1 * x[11:20])
+X <- seq(0, 1, length.out = 1000)
+Y1 <- predict(smooth.spline(x, y1, df = 10), X)$y
+Y2 <- 0.5 * X
+
+# PLOT -------------------------------------------------------------------------
+
+pdf("../figure/eval_ofit_3.pdf", width = 8, height = 3)
+
+par(mar = c(2.1, 2.1, 0, 0))
+
+plot(X, Y1, type = "l", axes = FALSE, xlab = "Complexity", ylab = "Error")
+mtext("Complexity", side = 1, line = 1)
+mtext("Error", side = 2, line = 1)
+
+lines(X, Y1 + Y2)
+abline(v = 0.42, lty = 2)
+text(0.4, 0.93, "Underfitting", pos = 2)
+text(0.44, 0.93, "Overfitting", pos = 4)
+arrows(0.4, 0.98, 0.2, 0.98, length = 0.1)
+arrows(0.44, 0.98, 0.64, 0.98, length = 0.1)
+box()
+text(0.85, 0.13, "Apparent error")
+text(0.85, 0.55, "Actual error")
+
+ggsave("../figure/eval_ofit_3.pdf", width = 8, height = 3)
+dev.off()
