2fc980fbadc1b4a0d3a968849815218975e0d761
Lisa Wimmer <wml1293@outlook.de>
2020-09-29 15:10:10 +0200

    replaced figures in evaluation-measures-classification-roc-space

--- slides/evaluation/rsrc/fig-eval_mclass_roc_sp_1.R
+++ slides/evaluation/rsrc/fig-eval_mclass_roc_sp_1.R
@@ -1,44 +1,73 @@
- 
+# PREREQ -----------------------------------------------------------------------
+
 library(knitr)
-library(mlbench)
-library(mlr)
-library(OpenML)
-
 library(ggplot2)
-library(viridis)
-library(gridExtra)
 library(ggrepel)
-library(repr)
 
-library(data.table)
-library(BBmisc)
-
-
-library(party)
-library(kableExtra)
-library(kknn)
-library(e1071)
-
-options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
-
-
-plot_lp = function(...){
-  plotLearnerPrediction(...) + scale_fill_viridis_d()
-}
+options(digits = 3, 
+        width = 65, 
+        str = strOptions(strict.width = "cut", vec.len = 3))
 
 source("plot_roc_space.R")
-pdf("../figure/eval_mclass_roc_sp_1.pdf", width = 4, height = 4)
 
+# PLOT 1 -----------------------------------------------------------------------
+
+pdf("../figure/eval_mclass_roc_sp_1.pdf", width = 4, height = 4)
 
 fpr = c(0.1, 0.30, 0.4)
 tpr = c(0.7, 0.85, 0.6)
 label = c("C1", "C2", "C3")
+
 pl = plot_roc_space(fpr, tpr, label)
-pl = pl + geom_segment(x = fpr[1], y = tpr[1], xend = fpr[2], yend = tpr[2], lty = "dotted")
-pl = pl + geom_text_repel(data = data.frame(FPR = 0.20, TPR = 0.83, label = "unclear winner"))
-pl = pl + geom_segment(x = fpr[1], y = tpr[1], xend = fpr[3], yend = tpr[3], lty = "dotted")
-pl = pl + geom_text_repel(data = data.frame(FPR = 0.25, TPR = 0.65, label = "dominates"))
+pl = pl + geom_segment(x = fpr[1], 
+                       y = tpr[1], 
+                       xend = fpr[2], 
+                       yend = tpr[2], 
+                       lty = "dotted")
+pl = pl + geom_text_repel(data = data.frame(FPR = 0.20, TPR = 0.83, 
+                                            label = "unclear winner"))
+pl = pl + geom_segment(x = fpr[1], 
+                       y = tpr[1], 
+                       xend = fpr[3], 
+                       yend = tpr[3], 
+                       lty = "dotted")
+pl = pl + geom_text_repel(data = data.frame(FPR = 0.25, TPR = 0.65, 
+                                            label = "dominates"))
 print(pl)
+
 ggsave("../figure/eval_mclass_roc_sp_1.pdf", width = 4, height = 4)
 dev.off()
 
+# PLOT 2 -----------------------------------------------------------------------
+
+pdf("../figure/eval_mclass_roc_sp_2.pdf", width = 4, height = 4)
+
+fpr = c(0, 1, 0, 0.25, 0.75)
+tpr = c(1, 1, 0, 0.25, 0.75)
+label = c("Best", "Pos-100%", "Pos-0%", "Pos-25%", "Pos-75%")
+
+pl = plot_roc_space(fpr, tpr, label)
+print(pl)
+
+ggsave("../figure/eval_mclass_roc_sp_2.pdf", width = 4, height = 4)
+dev.off()
+
+# PLOT 3 -----------------------------------------------------------------------
+
+pdf("../figure/eval_mclass_roc_sp_3.pdf", width = 4, height = 4)
+
+fpr = c(0.75, 0.25)
+tpr = c(0.25, 0.75)
+label = c("C1", "C2")
+
+pl = plot_roc_space(fpr, tpr, label)
+pl = pl + geom_segment(x = 
+                         fpr[1], 
+                       y = tpr[1], 
+                       xend = fpr[2], 
+                       yend = tpr[2], 
+                       lty = "dotted")
+print(pl)
+
+ggsave("../figure/eval_mclass_roc_sp_3.pdf", width = 4, height = 4)
+dev.off()
--- slides/evaluation/rsrc/fig-eval_mclass_roc_sp_3.R
+++ slides/evaluation/rsrc/fig-eval_mclass_roc_sp_3.R
@@ -1,181 +1,45 @@
- 
-library(knitr)
-library(mlbench)
-library(mlr)
-library(OpenML)
+# PREREQ -----------------------------------------------------------------------
 
+library(knitr)
 library(ggplot2)
 library(viridis)
-library(gridExtra)
-library(ggrepel)
-library(repr)
 
-library(data.table)
-library(BBmisc)
+# FUNCTIONS --------------------------------------------------------------------
 
-
-library(party)
-library(kableExtra)
-library(kknn)
-library(e1071)
-
-options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
-
-
-plot_lp = function(...){
-  plotLearnerPrediction(...) + scale_fill_viridis_d()
+plot_roc_curves = function(d) {
+  
+  pl = ggplot(d, aes(x = FPR, y = TPR, col = model)) +  
+    scale_color_viridis_d(direction = -1, end = .9)
+  pl = pl + geom_line(size = 2)
+  pl = pl + xlim(c(0, 1)) + ylim(c(0,1))
+  pl = pl + geom_abline(slope = 1, lty = "dotted")
+  
 }
 
-plotROC = function (df_auc, threshold, table = TRUE, auc = FALSE, highlight = TRUE) {
-  
-  bg_col_base = rep(c("#F2F2F2", "#CCCCCC"), length.out = nrow(df_auc))
-  bg_col_base[df_auc$Score > threshold] = "#6BAED6"
-  
-  theme = ttheme_default(
-    core = list(
-      bg_params = list(fill = bg_col_base, alpha = rep(c(1, 0.5), length.out = nrow(df_auc)))
-    )
-  )
-  
-  if (table)
-    p1 = gridExtra::tableGrob(df_auc, rows = NULL, theme = theme)
-  
-  n_pos = sum(df_auc$Truth == "Pos")
-  n_neg = sum(df_auc$Truth == "Neg")
-  
-  fpr = cumsum(df_auc$Truth == "Neg") / n_neg
-  tpr = cumsum(df_auc$Truth == "Pos") / n_pos
-  
-  
-  plt_idx = df_auc$Score > threshold
-  
-  df_plot = data.frame(
-    x = c(0, fpr[plt_idx]),
-    y = c(0, tpr[plt_idx])
-  )
-  
-  df_poly = df_plot[c(seq_len(nrow(df_plot)), rev(seq_len(nrow(df_plot)))), ]
-  df_poly$y[nrow(df_plot) + seq_len(nrow(df_plot))] = 0
-  
-  
-  p2 = ggplot(df_plot, aes(x = x, y = y))
-  
-  if (auc)
-    p2 = p2 + geom_polygon(data = df_poly, aes(x, y), fill = "#6BAED6", alpha = 0.3)
-  
-  p2 = p2 + geom_abline(intercept = 0, slope = 1, size = 1.3, linetype = 2, color = "gray") +
-    geom_line(size = 1.3, color = "#6BAED6", alpha = 0.5) +
-    geom_point(size = 3, shape = 23, fill = "#6BAED6", color = "#6BAED6") +
-    xlab("False Positive Rate") +
-    ylab("True Positive Rate") +
-    xlim(0, 1) +
-    ylim(0, 1)
-  
-  if (highlight)
-    p2 = p2 + geom_point(data = df_plot[nrow(df_plot), ], aes(x, y), size = 3, shape = 23, fill = "#C67171", color = "#C67171")
-  
-  if (table)
-    return(gridExtra::grid.arrange(p1, p2, nrow = 1, widths = c(1,2)))
-  
-  return(p2)
-}
+# DATA -------------------------------------------------------------------------
 
+# set.seed(123)
 
-#first figure 
+n = 10
+fpr = seq(0, 1, length.out = n)
+tpr1 = pbeta(fpr, shape1 = 0.3, shape2 = 9)
+tpr2 = pbeta(fpr, shape1 = 1, shape2 = 3)
+tpr3 = pbeta(fpr, shape1 = 2, shape2 = 5)
+tpr4 = pbeta(fpr, shape1 = 0.9, shape2 = 1)
 
-pdf("../figure/eval_mclass_roc_sp_4.pdf", width = 4, height = 4)
-df_auc = data.frame(
-  '#' = 1:12,
-  Truth = c("Pos", "Neg")[c(1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2)],
-  Score = c(0.95, 0.86, 0.69, 0.65, 0.59, 0.52, 0.51, 0.39, 0.28, 0.18, 0.15, 0.06)
+d = rbind(
+  data.frame(FPR = fpr, TPR = tpr1, model = "very good"),
+  data.frame(FPR = fpr, TPR = tpr2, model = "ok2"),
+  data.frame(FPR = fpr, TPR = tpr3, model = "ok1"),
+  data.frame(FPR = fpr, TPR = tpr4, model = "bad")
 )
-names(df_auc) = c("#", "Truth", "Score")
 
+# PLOT -------------------------------------------------------------------------
 
-plotROC(df_auc, 0, highlight = FALSE, table = FALSE)
-ggsave("../figure/eval_mclass_roc_sp_4.pdf", width = 4, height = 4)
+pdf("../figure/eval_mclass_roc_sp_12.pdf", width = 4.5, height = 3)
+
+pl = plot_roc_curves(d)
+print(pl)
+
+ggsave("../figure/eval_mclass_roc_sp_12.pdf", width = 4.5, height = 3)
 dev.off()
-
-
-#second figure with thresh = 0.9
-
-pdf("../figure/eval_mclass_roc_sp_5.pdf", width = 6, height = 4)
-
-plotROC(df_auc, thresh)
-ggsave("../figure/eval_mclass_roc_sp_5.pdf", width = 6, height = 4)
-dev.off()
-
-
-#third figure with thresh = 0.85
-
-pdf("../figure/eval_mclass_roc_sp_6.pdf", width = 6, height = 4)
-
-thresh = 0.85
-tpr = mlr::measureTPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos")
-fpr = mlr::measureFPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos", negative = "Neg")
-
-plotROC(df_auc, thresh)
-ggsave("../figure/eval_mclass_roc_sp_6.pdf", width = 6, height = 4)
-dev.off()
-
-
-#fourth figure with thresh = 0.66
-
-pdf("../figure/eval_mclass_roc_sp_7.pdf", width = 6, height = 4)
-
-thresh = 0.66
-tpr = mlr::measureTPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos")
-fpr = mlr::measureFPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos", negative = "Neg")
-
-plotROC(df_auc, thresh)
-ggsave("../figure/eval_mclass_roc_sp_7.pdf", width = 6, height = 4)
-dev.off()
-
-
-#fifth figure with thresh = 0.6
-
-pdf("../figure/eval_mclass_roc_sp_8.pdf", width = 6, height = 4)
-
-thresh = 0.6
-tpr = mlr::measureTPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos")
-fpr = mlr::measureFPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos", negative = "Neg")
-
-plotROC(df_auc, thresh)
-ggsave("../figure/eval_mclass_roc_sp_8.pdf", width = 6, height = 4)
-dev.off()
-
-
-#sixth figure with thresh = 0.55
-
-pdf("../figure/eval_mclass_roc_sp_9.pdf", width = 6, height = 4)
-
-thresh = 0.55
-tpr = mlr::measureTPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos")
-fpr = mlr::measureFPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos", negative = "Neg")
-
-plotROC(df_auc, thresh)
-ggsave("../figure/eval_mclass_roc_sp_9.pdf", width = 6, height = 4)
-dev.off()
-
-
-#seventh figure with thresh = 0.3
-
-pdf("../figure/eval_mclass_roc_sp_10.pdf", width = 6, height = 4)
-
-thresh = 0.3
-tpr = mlr::measureTPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos")
-fpr = mlr::measureFPR(truth = df_auc$Truth, response = ifelse(df_auc$Score > thresh, "Pos", "Neg"), positive = "Pos", negative = "Neg")
-
-plotROC(df_auc, thresh)
-ggsave("../figure/eval_mclass_roc_sp_10.pdf", width = 6, height = 4)
-dev.off()
-
-
-#eighth figure 
-
-pdf("../figure/eval_mclass_roc_sp_11.pdf", width = 6, height = 4)
-
-plotROC(df_auc, 0, highlight = FALSE)
-ggsave("../figure/eval_mclass_roc_sp_11.pdf", width = 6, height = 4)
-dev.off()
-
