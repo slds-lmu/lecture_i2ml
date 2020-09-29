3956812e68c191bca399b016898e534188ec3095
Lisa Wimmer <wml1293@outlook.de>
2020-09-29 17:59:57 +0200

    replaced utility function draw-cart-on-iris

--- slides/trees/rsrc/draw-cart-iris.R
+++ slides/trees/rsrc/draw-cart-iris.R
@@ -1,8 +1,21 @@
+library(mlr3)
+library(mlr3viz)
+library(ggplot2)
+library(rattle)
+library(viridis)
+
 draw_cart_on_iris = function(depth, with_tree_plot = FALSE) {
-  task = makeClassifTask(data = iris[,3:5], target = "Species")
-  lrn = makeLearner("classif.rpart", cp = 0, minbucket = 4, maxdepth = depth)
-  model = train(lrn, task)
-  p = plotLearnerPrediction(lrn, task, gridsize = 100, cv = 0, prob.alpha = FALSE, err.mark = "train")
+  
+  task = tsk("iris")
+  task$select(c("Petal.Width", "Petal.Length"))
+  
+  learner = lrn("classif.rpart", cp = 0, minbucket = 4, maxdepth = depth)
+  
+  learner$train(task)
+  model = learner$model
+  
+  p = plot_learner_prediction(learner, task)
+  p = p + guides(shape = FALSE)
   p = p + 
     theme(axis.line = element_line(colour = "black"),
           panel.grid.major = element_line(colour = "grey80"),
@@ -11,8 +24,18 @@ draw_cart_on_iris = function(depth, with_tree_plot = FALSE) {
           panel.background = element_blank()) + 
     ggtitle("Iris Data")
   print(p)
-  if (with_tree_plot)
-    # "palettes" should ensure that colors in tree growing in rectangles and tree match
-    fancyRpartPlot(model$learner.model, sub = "", palettes = c("Reds", "Greens", "Blues"))
+  
+  if (with_tree_plot) {
+    
+    # "palettes" should ensure that colors in tree growing in rectangles and 
+    # tree match
+    
+    fancyRpartPlot(model, 
+                   sub = "", 
+                   palettes = c("Reds", "Greens", "Blues"))
+    
+  }
+    
   invisible(model)
+  
 }
