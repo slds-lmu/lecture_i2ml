ae2a5f0f65a2ad83df43e98ed944eeea9206fc18
Lisa Wimmer <wml1293@outlook.de>
2020-09-29 18:06:04 +0200

    replaced figures in cart-splitcriteria

--- slides/trees/rsrc/fig-cart_splitcriteria_1.R
+++ slides/trees/rsrc/fig-cart_splitcriteria_1.R
@@ -1,59 +1,96 @@
- 
+# PREREQ -----------------------------------------------------------------------
+
 library(knitr)
-library(mlbench)
-library(mlr)
-library(OpenML)
-
 library(ggplot2)
-library(viridis)
-library(gridExtra)
-library(ggrepel)
-library(repr)
+library(tidyverse)
 
-library(data.table)
-library(BBmisc)
+# DATA -------------------------------------------------------------------------
 
+set.seed(1221)
+n = 50
+data = data.frame(x = seq(-4 , 2, l = n)) %>% 
+  mutate(y = ifelse(x < 0, 
+                    2.5 + rnorm(n), 
+                    2 - 3 * plogis(x) + .5 * rnorm(n)))
 
-library(party)
-library(kableExtra)
-library(kknn)
-library(e1071)
+datal = subset(data, x < 0)
+datar = subset(data, x > 0)
 
-options(digits = 3, width = 65, str = strOptions(strict.width = "cut", vec.len = 3))
+# PLOT -------------------------------------------------------------------------
 
+pdf("../figure/cart_splitcriteria_2.pdf", width = 8, height = 3)
 
-draw_cart_on_iris = function(depth, with_tree_plot = FALSE) {
-  task = makeClassifTask(data = iris[,3:5], target = "Species")
-  lrn = makeLearner("classif.rpart", cp = 0, minbucket = 4, maxdepth = depth)
-  model = train(lrn, task)
-  p = plotLearnerPrediction(lrn, task, gridsize = 100, cv = 0, prob.alpha = FALSE, err.mark = "train")
-  p = p + 
-    theme(axis.line = element_line(colour = "black"),
-          panel.grid.major = element_line(colour = "grey80"),
-          panel.grid.minor = element_line(colour = "grey80"),
-          panel.border = element_blank(),
-          panel.background = element_blank()) + 
-    ggtitle("Iris Data")
-  print(p)
-  if (with_tree_plot)
-    # "palettes" should ensure that colors in tree growing in rectangles and tree match
-    fancyRpartPlot(model$learner.model, sub = "", palettes = c("Reds", "Greens", "Blues"))
-  invisible(model)
-}
+p1 = ggplot(data) + 
+  geom_point(aes(x, y)) + 
+  geom_segment(
+    aes(x = min(x), xend = max(x), y = mean(y), yend = mean(y)), col = "red") +
+  geom_point(aes(x, y), alpha = .5) + 
+  theme_light(base_size = 16) + 
+  scale_y_continuous(breaks = mean(data$y), labels = "c", ) +
+  scale_x_continuous(expression(x[j]), 
+                     breaks = NULL,
+                     minor_breaks = NULL) + 
+  theme(axis.text.y = element_text(colour = "red"),
+        panel.grid.major = element_line(colour = NA),
+        plot.margin = unit(c(3, 3, 1, 1), "lines")) +
+  annotation_custom(
+    grob = grid::textGrob(label = "\uD835\uDCA9", 
+                          gp = grid::gpar(fontsize = 18)), 
+    ymin =  max(data$y) + 1,
+    ymax =  max(data$y) + 1,
+    xmin = mean(data$x),
+    xmax = mean(data$x)) +
+  annotation_custom(
+    grob = grid::textGrob(label = "\u21E8", 
+                          gp = grid::gpar(fontsize = 28)), 
+    ymin =  mean(data$y),
+    ymax =  mean(data$y) ,
+    xmin = max(data$x) + 1,
+    xmax = max(data$x) + 1)  +
+  coord_cartesian(clip = 'off')
 
+p2 = ggplot(data) + 
+  geom_point(aes(x, y)) + 
+  geom_segment(data = datal, 
+               aes(x = min(x), xend = 0, y = mean(y), yend = mean(y)),
+               col = "red") +
+  geom_segment(data = datar, 
+               aes(x = max(x), xend = 0, y = mean(y), yend = mean(y)),
+               col = "red") +
+  geom_point(aes(x,y), alpha = .5) + 
+  theme_light(base_size = 16) + 
+  scale_y_continuous(breaks = c(mean(datal$y), mean(datar$y)), 
+                     minor_breaks = NULL,
+                     labels = c(expression(c[1]), expression(c[2]))) +
+  scale_x_continuous(expression(x[j]), 
+                     breaks = NULL) + 
+  theme(axis.text.y = element_text(colour = "red"),
+        panel.grid.major = element_line(colour = NA),
+        plot.margin = unit(c(3, 1, 1, 1), "lines")) +
+  annotation_custom(
+    grob =  grid::textGrob(label = expression("\uD835\uDCA9"[1]),
+                           gp = grid::gpar(fontsize = 18)), 
+    ymin =  max(data$y) + 1,
+    ymax =  max(data$y) + 1,
+    xmin = mean(datal$x),
+    xmax = mean(datal$x)) +
+  annotation_custom(
+    grob = grid::textGrob(label = expression("\uD835\uDCA9"[2]),
+                          gp = grid::gpar(fontsize = 18)), 
+    ymin =  max(data$y) + 1,
+    ymax =  max(data$y) + 1,
+    xmin = mean(datar$x),
+    xmax = mean(datar$x)) +
+  geom_vline(xintercept = 0, lty = 3, alpha = .5) +
+  annotation_custom(
+    grob = grid::textGrob(label = "t"), 
+    ymin =  min(data$y) - .5,
+    ymax =  min(data$y) - .5,
+    xmin = 0,
+    xmax = 0) +
+  coord_cartesian(clip = 'off')
 
-scale_c_d <- scale_colour_discrete <- scale_color_discrete <-
-  function(...) {
-    viridis::scale_color_viridis(..., end = .9, discrete = TRUE, drop = TRUE)
-  }
+gridExtra::grid.arrange(p1, p2, nrow = 1, widths = c(1.2, 1))
 
-
-library(plyr)
-library(kernlab)
-set.seed(600000)
-pdf("../figure/cart_splitcriteria_1.pdf", width = 8, height = 4)
-model = draw_cart_on_iris(depth = 2)
-
-ggsave("../figure/cart_splitcriteria_1.pdf", width = 8, height = 4)
+ggsave("../figure/cart_splitcriteria_2.pdf", width = 8, height = 3)
 dev.off()
-
