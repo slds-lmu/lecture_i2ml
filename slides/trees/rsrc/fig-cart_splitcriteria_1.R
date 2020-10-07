ab244f4d18bba51a6b48a07c7b4ea7fd17e6ae7e
Tobias Pielok <t.pielok@gmail.com>
2020-10-07 15:20:29 +0200

    Enable mathcal fonts in plot

--- slides/trees/rsrc/fig-cart_splitcriteria_1.R
+++ slides/trees/rsrc/fig-cart_splitcriteria_1.R
@@ -3,6 +3,12 @@
 library(knitr)
 library(ggplot2)
 library(tidyverse)
+library(tikzDevice)
+
+options(tikzMetricPackages = c("\\usepackage[utf8]{inputenc}",
+                               "\\usepackage[T1]{fontenc}", "\\usetikzlibrary{calc}",
+                               "\\usepackage{amssymb}"))
+
 
 # DATA -------------------------------------------------------------------------
 
@@ -18,8 +24,20 @@ datar = subset(data, x > 0)
 
 # PLOT -------------------------------------------------------------------------
 
-pdf("../figure/cart_splitcriteria_2.pdf", width = 8, height = 3)
+fname = "cart_splitcriteria_2"
+tex_fname = paste0(fname, ".tex")
+pdf_fname = paste0(fname, ".pdf")
+dest_pdf =  paste0("../figure/", pdf_fname)
 
+# In order to get mathcal fonts to work system-independently we create a 
+# temporary tex-file. For more information see 
+# https://yihui.org/en/2011/04/produce-authentic-math-formulas-in-r-graphics/
+tikz(tex_fname, width = 8, height = 3, standAlone = TRUE,
+     packages = c("\\usepackage{tikz}",
+                  "\\usepackage[active,tightpage,psfixbb]{preview}",
+                  "\\PreviewEnvironment{pgfpicture}",
+                  "\\setlength\\PreviewBorder{0pt}",
+                  "\\usepackage{amssymb}"))
 p1 = ggplot(data) + 
   geom_point(aes(x, y)) + 
   geom_segment(
@@ -27,21 +45,21 @@ p1 = ggplot(data) +
   geom_point(aes(x, y), alpha = .5) + 
   theme_light(base_size = 16) + 
   scale_y_continuous(breaks = mean(data$y), labels = "c", ) +
-  scale_x_continuous(expression(x[j]), 
+  scale_x_continuous("$x_j$", 
                      breaks = NULL,
                      minor_breaks = NULL) + 
   theme(axis.text.y = element_text(colour = "red"),
         panel.grid.major = element_line(colour = NA),
         plot.margin = unit(c(3, 3, 1, 1), "lines")) +
   annotation_custom(
-    grob = grid::textGrob(label = "\uD835\uDCA9", 
+    grob = grid::textGrob(label = "$\\mathcal{N}$", 
                           gp = grid::gpar(fontsize = 18)), 
     ymin =  max(data$y) + 1,
     ymax =  max(data$y) + 1,
     xmin = mean(data$x),
     xmax = mean(data$x)) +
   annotation_custom(
-    grob = grid::textGrob(label = "\u21E8", 
+    grob = grid::textGrob(label = "$\\Rightarrow$", 
                           gp = grid::gpar(fontsize = 28)), 
     ymin =  mean(data$y),
     ymax =  mean(data$y) ,
@@ -61,21 +79,21 @@ p2 = ggplot(data) +
   theme_light(base_size = 16) + 
   scale_y_continuous(breaks = c(mean(datal$y), mean(datar$y)), 
                      minor_breaks = NULL,
-                     labels = c(expression(c[1]), expression(c[2]))) +
-  scale_x_continuous(expression(x[j]), 
+                     labels = c("$c_1$", "$c_2$")) +
+  scale_x_continuous("$x_j$", 
                      breaks = NULL) + 
   theme(axis.text.y = element_text(colour = "red"),
         panel.grid.major = element_line(colour = NA),
         plot.margin = unit(c(3, 1, 1, 1), "lines")) +
   annotation_custom(
-    grob =  grid::textGrob(label = expression("\uD835\uDCA9"[1]),
+    grob =  grid::textGrob(label = "$\\mathcal{N}_1$",
                            gp = grid::gpar(fontsize = 18)), 
     ymin =  max(data$y) + 1,
     ymax =  max(data$y) + 1,
     xmin = mean(datal$x),
     xmax = mean(datal$x)) +
   annotation_custom(
-    grob = grid::textGrob(label = expression("\uD835\uDCA9"[2]),
+    grob = grid::textGrob(label = "$\\mathcal{N}_2$",
                           gp = grid::gpar(fontsize = 18)), 
     ymin =  max(data$y) + 1,
     ymax =  max(data$y) + 1,
@@ -92,5 +110,11 @@ p2 = ggplot(data) +
 
 gridExtra::grid.arrange(p1, p2, nrow = 1, widths = c(1.2, 1))
 
-ggsave("../figure/cart_splitcriteria_2.pdf", width = 8, height = 3)
 dev.off()
+tools::texi2pdf(tex_fname, clean = TRUE)
+unlink(dest_pdf)
+file.copy(pdf_fname, dest_pdf)
+
+# clean-up
+unlink(pdf_fname)
+unlink(tex_fname)
