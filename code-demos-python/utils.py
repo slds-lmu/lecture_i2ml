cf859390139f0396cf1ad29dbfd084559b7b04c7
maveriq <harisjabbar@gmail.com>
2020-11-16 21:44:08 +0100

    demos commit

--- code-demos-python/utils.py
+++ code-demos-python/utils.py
@@ -1,4 +1,7 @@
 import streamlit as st 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 def _page_width_(width):
     max_width_str = f"max-width: {width}px;"
@@ -14,3 +17,133 @@ def _page_width_(width):
     )
 
 float_formatter = "{:.4f}".format
+
+def abline(slope, intercept):
+    """Plot a line from slope and intercept"""
+    axes = plt.gca()
+    xlim=axes.get_xlim()
+    ylim=axes.get_ylim()
+    x_vals = np.array(xlim)
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--')
+    plt.ylim(ylim)
+
+def BacktrackingLineSearch(f, df, x, p, df_x = None, f_x = None, args = (),
+        alpha = 0.0001, beta = 0.9, eps = 1e-15, Verbose = False):
+    """
+    source : https://gist.github.com/jiahao/1561144
+    
+    Backtracking linesearch
+    f: function
+    df: gradient function
+    x: current point
+    p: direction of search
+    df_x: gradient at x
+    f_x = f(x) (Optional)
+    args: optional arguments to f (optional)
+    alpha, beta: backtracking parameters
+    eps: (Optional) quit if norm of step produced is less than this
+    Verbose: (Optional) Print lots of info about progress
+    
+    Reference: Nocedal and Wright 2/e (2006), p. 37
+    
+    Usage notes:
+    -----------
+    Recommended for Newton methods; less appropriate for quasi-Newton or conjugate gradients
+    """
+
+    if f_x is None:
+        f_x = f(x, *args)
+    if df_x is None:
+        df_x = df(x, *args)
+
+    assert df_x.T.shape == p.shape
+    assert 0 < alpha < 1, 'Invalid value of alpha in backtracking linesearch'
+    assert 0 < beta < 1, 'Invalid value of beta in backtracking linesearch'
+
+    derphi = np.dot(df_x, p)
+
+    assert derphi.shape == (1, 1) or derphi.shape == ()
+    assert derphi < 0, 'Attempted to linesearch uphill'
+
+    stp = 1.0
+    fc = 0
+    len_p = np.linalg.norm(p)
+
+
+    #Loop until Armijo condition is satisfied
+    while f(x + stp * p, *args) > f_x + alpha * stp * derphi:
+        stp *= beta
+        fc += 1
+        if Verbose: print('linesearch iteration', fc, ':', stp, f(x + stp * p, *args), f_x + alpha * stp * derphi)
+        if stp * len_p < eps:
+            print('Step is  too small, stop')
+            break
+    #if Verbose: print 'linesearch iteration 0 :', stp, f_x, f_x
+
+    if Verbose: print('linesearch done')
+    #print fc, 'iterations in linesearch'
+    return stp
+
+def plot_decision_boundaries(X, y, model_class, **model_params):
+    """
+    source : https://github.com/tirthajyoti/Machine-Learning-with-Python/blob/master/Utilities/ML-Python-utils.py
+    
+    Function to plot the decision boundaries of a classification model.
+    This uses just the first two columns of the data for fitting 
+    the model as we need to find the predicted value for every point in 
+    scatter plot.
+    Arguments:
+            X: Feature data as a NumPy-type array.
+            y: Label data as a NumPy-type array.
+            model_class: A Scikit-learn ML estimator class 
+            e.g. GaussianNB (imported from sklearn.naive_bayes) or
+            LogisticRegression (imported from sklearn.linear_model)
+            **model_params: Model parameters to be passed on to the ML estimator
+    
+    Typical code example:
+            plt.figure()
+            plt.title("KNN decision boundary with neighbros: 5",fontsize=16)
+            plot_decision_boundaries(X_train,y_train,KNeighborsClassifier,n_neighbors=5)
+            plt.show()
+    """
+    try:
+        X = np.array(X)
+        y = np.array(y).flatten()
+    except:
+        print("Coercing input data to NumPy arrays failed")
+    # Reduces to the first two columns of data
+    reduced_data = X[:, :2]
+    # Instantiate the model object
+    model = model_class(**model_params)
+    # Fits the model with the reduced data
+    model.fit(reduced_data, y)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].    
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+    y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+    # Meshgrid creation
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point in mesh using the model.
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])    
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                         np.arange(y_min, y_max, 0.1))
+
+    # Predictions to obtain the classification results
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    # Plotting
+    plt.contourf(xx, yy, Z, alpha=0.4)
+    plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.8)
+    plt.xlabel("Feature-1",fontsize=15)
+    plt.ylabel("Feature-2",fontsize=15)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    return plt
