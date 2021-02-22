b09a49266d8af3e22dc8d252d6d59eeff630c411
Caro <carolin.becker@campus.lmu.de>
2021-02-22 22:32:55 +0100

    Update test-holdout-example.R

--- slides/evaluation/rsrc/test-holdout-example.R
+++ slides/evaluation/rsrc/test-holdout-example.R
@@ -30,3 +30,13 @@ pl1 = pl1 + theme(axis.text.x = element_text(angle = 45))
 print(pl1)
 
 ggsave("figure/test-holdout-example.pdf", width = 8, height = 3.5)  
+
+
+pl2 = ggplot(gmse[gmse$type == "holdout", ], aes(x = split, y = mse))
+pl2 = pl2 + geom_line()
+pl2 = pl2 + scale_y_log10()
+pl2 = pl2 + scale_x_continuous(breaks = gmse$split)
+pl2 = pl2 + theme(axis.text.x = element_text(angle = 45))
+print(pl2)
+
+ggsave("figure/test-holdout-example-2.pdf", width = 8, height = 3.5)  
