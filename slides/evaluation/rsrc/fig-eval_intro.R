3d36633324f9f348c431044ae71b03916bf93f47
Lisa Wimmer <wml1293@outlook.de>
2021-02-23 06:39:13 +0100

    changed wording a bit and adjusted zero one loss fig to have correct ax titles

--- slides/evaluation/rsrc/fig-eval_intro.R
+++ slides/evaluation/rsrc/fig-eval_intro.R
@@ -10,11 +10,11 @@ options(digits = 3,
 # DATA -------------------------------------------------------------------------
 
 my_df <- data.frame(
-  x_1 = -2, 
-  x_2 = 0, 
-  x_3 = 2, 
-  y_1 = 0, 
-  y_2 = 1 )
+  x_1 = -1, 
+  x_2 = 1, 
+  x_3 = 3, 
+  y_1 = 1, 
+  y_2 = 0 )
 
 # PLOT -------------------------------------------------------------------------
 
@@ -24,10 +24,10 @@ ggplot(my_df) +
   geom_segment(aes(x = x_1, xend = x_2, y = y_2, yend = y_2)) +
   geom_segment(aes(x = x_2, xend = x_2, y = y_1, yend = y_2)) +
   geom_segment(aes(x = x_2, xend = x_3, y = y_1, yend = y_1)) +
-  xlab("y - h(x)") +
-  ylab("L(y - h(x))") + 
+  xlab(as.expression(bquote("[y" != "h(x)]"))) +
+  ylab("L(y, h(x))") + 
   scale_y_continuous(breaks = seq(0, 1, by = 1)) +
-  scale_x_continuous(breaks = c(0)) +
+  scale_x_continuous(breaks = c(1)) +
   theme(
     axis.text = element_text(size = 30),
     axis.title = element_text(size = 30))
