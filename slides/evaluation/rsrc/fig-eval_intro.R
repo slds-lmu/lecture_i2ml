fa000081c8133814799c9343090ae6ba0c081866
Lisa Wimmer <wml1293@outlook.de>
2021-03-05 07:24:08 +0100

    corrected typos, wording, layout

--- slides/evaluation/rsrc/fig-eval_intro.R
+++ slides/evaluation/rsrc/fig-eval_intro.R
@@ -18,19 +18,17 @@ my_df <- data.frame(
 
 # PLOT -------------------------------------------------------------------------
 
-pdf("../figure/zero-one-loss.pdf", width = 8, height = 4)
-
-ggplot(my_df) +
-  geom_segment(aes(x = x_1, xend = x_2, y = y_2, yend = y_2)) +
-  geom_segment(aes(x = x_2, xend = x_2, y = y_1, yend = y_2)) +
-  geom_segment(aes(x = x_2, xend = x_3, y = y_1, yend = y_1)) +
+p <- ggplot(my_df) +
+  geom_segment(aes(x = x_1, xend = x_2, y = y_2, yend = y_2), size = 2L) +
+  geom_segment(aes(x = x_2, xend = x_2, y = y_1, yend = y_2), size = 2L) +
+  geom_segment(aes(x = x_2, xend = x_3, y = y_1, yend = y_1), size = 2L) +
   xlab(as.expression(bquote("[y" != "h(x)]"))) +
   ylab("L(y, h(x))") + 
-  scale_y_continuous(breaks = seq(0, 1, by = 1)) +
-  scale_x_continuous(breaks = c(1)) +
+  scale_y_continuous(breaks = seq(0L, 1L, by = 1L)) +
+  scale_x_continuous(breaks = c(1L)) +
   theme(
-    axis.text = element_text(size = 30),
-    axis.title = element_text(size = 30))
+    axis.text = element_text(size = 30L),
+    axis.title = element_text(size = 30L))
 
-ggsave("../figure/zero-one-loss.pdf", width = 8, height = 4)
+ggsave("../figure/zero-one-loss.pdf", p, width = 8, height = 4)
 dev.off()
