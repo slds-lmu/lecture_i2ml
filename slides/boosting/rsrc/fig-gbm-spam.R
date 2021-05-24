3697c0f6e2d0ad5325b31dbbe20b653985a0df5d
Herbinger <Julia.Herbinger@lmu.de>
2021-05-24 19:58:28 +0200

    update

--- slides/boosting/rsrc/fig-gbm-spam.R
+++ slides/boosting/rsrc/fig-gbm-spam.R
@@ -23,14 +23,14 @@ shrinkage_params <- c(0.001, 0.01, 0.1)
 tree_depth_params <- c(1L, 3L, 5L, 20L)
 
 learners <- lapply(
-  shrinkage_params, 
+  shrinkage_params,
   function(i) {
     sapply(
       tree_depth_params,
       function(j) {
         mlr3::lrn(
-          "classif.gbm", 
-          distribution = "bernoulli", 
+          "classif.gbm",
+          distribution = "bernoulli",
           n.trees = n_trees,
           cv.folds = 3L,
           shrinkage = i,
@@ -41,14 +41,14 @@ for (i in unlist(learners)) i$train(task)
 # PLOTS ------------------------------------------------------------------------
 
 errors <- lapply(
-  seq_along(shrinkage_params), 
+  seq_along(shrinkage_params),
   function(i) {
     lapply(
-      seq_along(tree_depth_params), 
+      seq_along(tree_depth_params),
       function(j) learners[[i]][[j]]$model$cv.error)})
 
 trajectories <- lapply(
-  seq_along(errors), 
+  seq_along(errors),
   function(i) {
     lapply(
       seq_along(tree_depth_params),
@@ -65,15 +65,17 @@ trajectories_dt$tree_depth <- as.factor(trajectories_dt$tree_depth)
 saveRDS(trajectories_dt, "fig-gbm-spam-data.RDS")
 
 p_1 <- ggplot2::ggplot(
-  trajectories_dt, 
+  trajectories_dt,
   ggplot2::aes(x = trees, y = errors, col = tree_depth)) +
   ggplot2::geom_line() +
-  ggplot2::theme_minimal() + 
+  ggplot2::theme_minimal() +
   ggplot2::ylim(c(0.1, 1.5)) +
   ggplot2::xlab("iterations") +
   ggplot2::ylab("mean classification error") +
   ggplot2::scale_color_viridis_d(end = 0.9, name = "tree depth") +
   ggplot2::facet_grid(cols = vars(shrinkage)) +
   ggplot2::ggtitle("shrinkage")
+
+# load("gbm_spam_results.RData")
 
 ggplot2::ggsave("../figure/gbm_spam.png", p_1, height = 3L, width = 8L)
