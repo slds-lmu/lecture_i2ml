9362c7a4115ff1fa120a221a6f9ee4896b9b0a37
Carolin <carolin.becker@campus.lmu.de>
2021-05-24 21:52:15 +0200

    add plot and non-linear regu

---
+++ slides/regularization/rsrc/fig-regu-nonlin.R
@@ -0,0 +1,71 @@
+################################################################################
+####### Non-linear regularization: Neural net ##################################
+################################################################################
+
+library(mlr3)
+library(mlr3learners)
+library(mlr3viz)
+
+library(ggplot2)
+
+library(gridExtra)
+library(grid)
+#-------------------------------------------------------------------------------
+options(digits = 3, 
+        width = 65, 
+        str = strOptions(strict.width = "cut", vec.len = 3))
+
+
+
+#-------------------------------------------------------------------------------
+# function for plotting the weights 
+
+plot_weights <- function (weights) {
+  weight_data <- data.frame(value = weights, weights = seq_along(weights))
+  
+  ggplot(weight_data, aes(x=weights, y = value)) + 
+    geom_bar(stat ="identity", color="black", fill="white") +
+    ylim(c(-75, 75)) +
+    ggtitle("Weights")
+}
+
+# function for plotting the prediction
+
+plot_prediction <- function (learner, task) {
+  plot_learner_prediction(learner, task) + 
+    scale_fill_viridis_d(end = .9) + 
+    theme(legend.position = "none") +
+    ggtitle("Prediction")
+}
+
+#-------------------------------------------------------------------------------
+#spirals dataset
+spirals_generator = tgen("spirals")
+
+# get spirals data 
+spirals_task = spirals_generator$generate(250) 
+
+# decay parameter / lambda 
+decay_list = list(0, 0.001, 0.005, 0.01, 0.1)
+
+# size of single hidden layer
+size = 10
+
+
+# plot for all decay paramters the predition & a plot of the weights
+for(i in seq_along(decay_list)){
+  set.seed(1234)
+  learner = lrn("classif.nnet", size = size, decay = decay_list[[i]])
+  
+  learner$train(spirals_task)
+  weight_plot <- plot_weights(weights = learner$model$wts) 
+  
+  prediction_plot <- plot_prediction(learner, spirals_task)
+  
+  grid <- grid.arrange(prediction_plot,weight_plot, ncol = 2, 
+                       top = textGrob(bquote(lambda==.(decay_list[[i]])), gp = gpar(fontsize = 20))) 
+  
+  ggsave(filename = paste0("../figure/fig-regu-nonlin-", i ,".png"), 
+         plot = grid, width = 8, height = 4.5) 
+  
+}
