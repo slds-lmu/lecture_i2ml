1bb756026402aa2da16dcc08ee75f654bef807d6
lisa-wm <wml1293@outlook.de>
2021-06-13 15:52:41 +0200

    added example delta train test error

--- slides/evaluation/rsrc/fig-eval_delta_train_test_error.R
+++ slides/evaluation/rsrc/fig-eval_delta_train_test_error.R
@@ -17,7 +17,6 @@ library(mlbench)
 
 set.seed(1L)
 data <- mlbench::mlbench.2dnormals(n = 100000L, cl = 2L, r = sqrt(2L), sd = 1L)
-# data <- mlbench::mlbench.spirals(n = 10000L, sd = 0.1, cycles = 2L)
 data_dt <- data.table::as.data.table(data)
 
 # Set aside large test dataset
@@ -31,7 +30,6 @@ data_train <- data_dt[setdiff(data_dt[, .I], idx_test)]
 
 # Define portions of training data to be used
 
-# train_sizes <- c(0.001, 0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1)
 train_sizes <- c(seq(5L, 50L, by = 5L), seq(100L, 10000L, by = 50L))
 
 # Define learner
@@ -83,10 +81,31 @@ for (i in names(results_dt)) results_dt[[i]] <- unlist(results_dt[[i]])
 
 # PLOTS ------------------------------------------------------------------------
 
-p <- ggplot2::ggplot(
+p_1 <- ggplot2::ggplot(
+  data_dt, ggplot2::aes(x = x.1, y = x.2, col = classes)) +
+  ggplot2::geom_jitter(size = 0.8) +
+  ggplot2::theme_minimal() +
+  ggplot2::scale_color_viridis_d(end = 0.9) +
+  ggplot2::xlab(expr(x[1])) +
+  ggplot2::ylab(expr(x[2])) +
+  ggplot2::guides(color = FALSE)
+
+ggplot2::ggsave(
+  "../figure/eval_delta_train_test_err_data.pdf", 
+  p_1, 
+  height = 2L,
+  width = 3.5)
+
+p_2 <- ggplot2::ggplot(
   results_dt, 
   ggplot2::aes(x = train_size, ymax = delta, ymin = 0)) +
   ggplot2::geom_linerange() +
   ggplot2::theme_minimal() +
   ggplot2::xlab("training set size") +
   ggplot2::ylab("test error - train error")
+
+ggplot2::ggsave(
+  "../figure/eval_delta_train_test_err.pdf", 
+  p_2, 
+  height = 2L,
+  width = 8L)
