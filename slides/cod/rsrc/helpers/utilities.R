a84bd968d681a3acb01783565c076d3d2c7f2c0d
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2021-11-28 22:34:02 +0100

    Cleaned up redundant image and updated utilities for source code from chapter 14.01: Curse of Dimensionality

--- slides/cod/rsrc/helpers/utilities.R
+++ slides/cod/rsrc/helpers/utilities.R
@@ -2,10 +2,18 @@ S_p <- function(p){
   2 * (pi^((p + 1)/2))/gamma((p + 1)/2)
 }
 
+V_p <- function(p) {
+  (pi^(p/2))/gamma(p/2 + 1)
+}
+
 V_p_r <- function(p, r) {
-  (pi^(p/2))/gamma(p/2 + 1) * r^p
+  V_p(p) * r^p
+}
+
+S_p_r <- function(p, r) {
+  S_p(p) * r^p
 }
 
 p_r <- function(r, p=2, sigma=1){
-  V_p_r(p, r) * p / r / ((2*pi*sigma^2)^(p/2)) * exp(-r^2/(2*sigma^2))
+  S_p_r(p - 1,r) / ((2*pi*sigma^2)^(p/2)) * exp(-r^2/(2*sigma^2))
 }
