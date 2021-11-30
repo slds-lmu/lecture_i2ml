63f24aa07cc29857d7138b51bae468eb867f5112
Bernd Bischl <bernd_bischl@gmx.net>
2021-11-30 20:26:47 +0100

    BB resa2 not dne

--- slides/evaluation/rsrc/eval-resampling-example.R
+++ slides/evaluation/rsrc/eval-resampling-example.R
@@ -31,9 +31,9 @@ pl2 = pl2 + ggplot2::geom_line()
 pl2 = pl2 + ggplot2::scale_y_log10()
 pl2 = pl2 + ggplot2::scale_x_continuous(breaks = gmse$split)
 pl2 = pl2 + ggplot2::theme(axis.text.x = element_text(angle = 45))
-pl2 = pl2 + ggplot2::ylab("MSE of the mce")
+pl2 = pl2 + ggplot2::ylab(expression(paste("MSE of ", widehat(GE))))
 pl2 = pl2 + ggplot2::scale_color_viridis_d(end = 0.5)
 print(pl2)
 
 ggplot2::ggsave(
-  "../figure/eval-resampling-example-2.pdf", width = 5, height = 3.5)
+  "../figure/eval-resampling-example-2.pdf", width = 7, height = 3.5)
