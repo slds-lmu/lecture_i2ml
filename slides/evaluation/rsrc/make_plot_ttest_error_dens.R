cd441b8ae07dc6b381de00c62d73a2200d5cad76
lisa-wm <wml1293@outlook.de>
2021-12-01 05:24:06 +0100

    added ex for ge ttest

--- slides/evaluation/rsrc/make_plot_ttest_error_dens.R
+++ slides/evaluation/rsrc/make_plot_ttest_error_dens.R
@@ -28,22 +28,28 @@ sd_svm <- sd(errors[learner_id == "classif.svm"]$classif.ce)
 
 # PLOT -------------------------------------------------------------------------
 
+# plot error densities
+
 p <- ggplot(errors, aes(x = classif.ce, fill = learner_id)) +
   geom_density(col = NA, alpha = 0.6) +
   xlim(0L, 0.2) +
   scale_fill_manual(
-    "learner", labels = c("LDA", "SVM"), values = c("black", "darkgray")) +
-  theme_classic() +
-  labs(x = "MCE", y = "density over 20-CV") +
+    "learner", labels = c("LDA", "SVM"), values = c("gray60", "gray20")) +
+  # theme_classic() +
+  labs(x = "MCE", y = "density over 20-CV")
+
+# annotate lines for mean and sd 
+
+p <- p +
   geom_segment(
-    x = dens_lda$x[which.max(dens_lda$y)], 
-    xend = dens_lda$x[which.max(dens_lda$y)], 
-    y = 0L, 
+    x = dens_lda$x[which.max(dens_lda$y)],
+    xend = dens_lda$x[which.max(dens_lda$y)],
+    y = 0L,
     yend = max(density(errors[learner_id == "classif.lda"]$classif.ce)$y)) +
   geom_segment(
-    x = dens_svm$x[which.max(dens_svm$y)], 
-    xend = dens_svm$x[which.max(dens_svm$y)], 
-    y = 0L, 
+    x = dens_svm$x[which.max(dens_svm$y)],
+    xend = dens_svm$x[which.max(dens_svm$y)],
+    y = 0L,
     yend = max(density(errors[learner_id == "classif.svm"]$classif.ce)$y)) +
   geom_segment(
     x = dens_lda$x[which.max(dens_lda$y)] - sd_lda,
@@ -57,13 +63,35 @@ p <- ggplot(errors, aes(x = classif.ce, fill = learner_id)) +
     yend = 3) +
   annotate(
     "text", 
-    x = 0.04, 
+    x = 0.03, 
     y = 5, 
-    label = bquote(sigma[SVM] ~ "=" ~ .(round(sd_svm, 3)))) +
+    label = list(bquote(sigma[SVM] ~ "=" ~ .(round(sd_svm, 3)))),
+    parse = TRUE) +
   annotate(
     "text", 
-    x = 0.13, 
+    x = 0.14, 
     y = 3, 
-    label = bquote(sigma[LDA] ~ "=" ~ .(round(sd_lda, 3)))) +
-  
-p
+    label = list(bquote(sigma[LDA] ~ "=" ~ .(round(sd_lda, 3)))),
+    parse = TRUE)
+
+# make bg transparent
+
+p <- p +
+  theme(
+    panel.background = element_rect(fill = "transparent"),
+    plot.background = element_rect(fill = "transparent"), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), 
+    legend.background = element_rect(fill = "transparent"))
+
+ggsave(
+  "../figure/plot_ttest_error_dens.png",
+  p,
+  height = 2.5,
+  width = 5L)
+
+# TEST -------------------------------------------------------------------------
+
+p_val <- t.test(
+  errors[learner_id == "classif.lda"]$classif.ce, 
+  errors[learner_id == "classif.svm"]$classif.ce)$p.value
