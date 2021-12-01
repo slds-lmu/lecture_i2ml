abcba89b3b22c3682e97921e4d115d585aec802e
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2021-12-01 12:06:26 +0100

    Updated utility functions for cod chapter

--- slides/cod/rsrc/helpers/utilities.R
+++ slides/cod/rsrc/helpers/utilities.R
@@ -1,5 +1,5 @@
 S_p <- function(p){
-  2 * (pi^((p + 1)/2))/gamma((p + 1)/2)
+  p * (pi^(p/2))/gamma(p/2 + 1)
 }
 
 V_p <- function(p) {
@@ -11,9 +11,9 @@ V_p_r <- function(p, r) {
 }
 
 S_p_r <- function(p, r) {
-  S_p(p) * r^p
+  S_p(p) * r^(p - 1)
 }
 
 p_r <- function(r, p=2, sigma=1){
-  S_p_r(p - 1,r) / ((2*pi*sigma^2)^(p/2)) * exp(-r^2/(2*sigma^2))
+  S_p_r(p,r) / ((2*pi*sigma^2)^(p/2)) * exp(-r^2/(2*sigma^2))
 }
