f0848bd66273575a4199562ed33d5cca4bd0d71f
lisa-wm <wml1293@outlook.de>
2021-12-03 09:47:01 +0100

    added pauc fig

--- slides/evaluation/rsrc/make_fig_pauc.R
+++ slides/evaluation/rsrc/make_fig_pauc.R
@@ -18,12 +18,13 @@ p <- ggplot() +
     mapping = aes(xmin = 0.25, xmax = 0.5, ymin = 0, ymax = 1), 
     fill = NA,
     col = "blue") +
-  theme_minimal() +
+  geom_segment(mapping = aes(x = 0, xend = 1, y = 0, yend = 1), linetype = 2) +
+  theme_classic() +
   labs(x = "FPR", y = "TPR")
 
 dt_highlight <- data.table(
   x = c(rep(0.25, 4), rep(0.5, 4)),
-  y = c(0, 0.3, 0.67, 1, 0, 0.4, 0.87, 1), 
+  y = c(0, 0.25, 0.67, 1, 0, 0.5, 0.87, 1), 
   text = c("A", "h", "D", "f", "B", "g", "C", "e"))
 
 p <- p + geom_label(
