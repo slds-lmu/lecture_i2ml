78fc103e4e08e13980f02fbf08fac8315f99aa34
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2021-12-08 18:09:27 +0100

    Updated overfitting visualization script.

--- slides/evaluation/rsrc/make_overfitting_dataset.R
+++ slides/evaluation/rsrc/make_overfitting_dataset.R
@@ -10,9 +10,9 @@ options(warn = -1)
 
 set.seed(0)
 
-min_n <- 10
+min_n <- 100
 min_d <- 10
-max_n <- 500
+max_n <- 5000
 max_d <- 500
 len_n <- 10
 len_d <- 10
@@ -39,10 +39,10 @@ make_regression_data <- function(d=500, n=500) {
 }
 
 make_task_data <- function(d=500, n=500) {
-  # peak <- mlbench.peak(n=n + n_test,d=d)
-  reg <- make_regression_data(d=d,n=n)
-  data <- as.data.frame(reg$X)
-  data$y <- reg$y
+  peak <- mlbench.peak(n=n + n_test,d=d)
+  # reg <- make_regression_data(d=d,n=n)
+  data <- as.data.frame(peak$x)
+  data$y <- peak$y
   task <- as_task_regr(data, target = "y", id = paste0(d, "_", n))
   task
 }
@@ -50,14 +50,17 @@ make_task_data <- function(d=500, n=500) {
 df <- data.frame(learner_id=character(),
                  sample=integer(),
                  dim=integer(),
+                 train_error=double(),
+                 test_error=double(),
                  of=double())
 measure <- msr("regr.mse")
 
 learners <- list(
   lrn("regr.lm", id="Linear Model"),
+  lrn("regr.rpart", id="Regression Tree"),
   lrn("regr.kknn", id="K-Nearest Neighbors"),
   lrn("regr.ranger", id="Random Forest"),
-  lrn("regr.xgboost", id="Gradient Boosting"),
+  lrn("regr.xgboost", id="Gradient Boosting", nthread=4, nrounds=10),
   lrn("regr.svm", id="Support Vector Machine")
 )
 
@@ -67,6 +70,8 @@ for (i in 1:length(dims)) {
     n <- sample_sizes[j]
     print(paste0("starting task w/ dim=", d, " & sample_size=", n))
     of_list <- list()
+    train_error_list <- list()
+    test_error_list <- list()
     for (iter in 1:n_iters) {
       task <- make_task_data(d=d,n=n)
       holdout <- rsmp("holdout", ratio=as.double(n)/(n_test + n))
@@ -78,8 +83,12 @@ for (i in 1:length(dims)) {
         of <- test_error - train_error
         if (learner$id %in% names(of_list)) {
           of_list[[learner$id]] <- of_list[[learner$id]] + of
+          train_error_list[[learner$id]] <- train_error_list[[learner$id]] + train_error
+          test_error_list[[learner$id]] <- test_error_list[[learner$id]] + test_error
         } else {
           of_list[[learner$id]] <- of
+          train_error_list[[learner$id]] <- train_error
+          test_error_list[[learner$id]] <- test_error
         }
         learner$reset()
       }
@@ -88,7 +97,10 @@ for (i in 1:length(dims)) {
       df <- rbind(df, list(learner_id=learner$id,
                        sample=n,
                        dim=d,
-                       of=of_list[[learner$id]]/n_iters))
+                       of=of_list[[learner$id]]/n_iters,
+                       train_error=train_error_list[[learner$id]]/n_iters,
+                       test_error=test_error_list[[learner$id]]/n_iters
+      ))
     }
     print(paste0("finished task w/ dim=", d, " & sample_size=", n))
   }
@@ -97,7 +109,7 @@ for (i in 1:length(dims)) {
 
 # saveRDS(df, file = "overfiting_mlr.rds")
 
-saveRDS(df, file = "overfitting_mlr.rds")
+saveRDS(df, file = "overfitting_peak_boost_10rounds.rds")
 
 options(warn = defaultW)
 
--- slides/evaluation/rsrc/make_visualize_overfitting_plot.R
+++ slides/evaluation/rsrc/make_visualize_overfitting_plot.R
@@ -1,23 +1,30 @@
 library(ggplot2)
 library(viridis)
 library(gridExtra)
+library(scales)
 
 theme_set(theme_minimal())
 
-df <- readRDS("overfitting_peak.rds")
+df <- readRDS("overfitting_peak_boost_10rounds.rds")
 
-draw_of_heatmap <- function(learner_id) {
-  ggplot(df[df$learner_id==learner_id,], aes(dim, sample, fill=of)) +
-    scale_x_continuous(trans = "log2") +
-    scale_y_continuous(trans = "log2") +
-    geom_tile() +
-    scale_fill_viridis(end=0.9, name = expression(OF(hat(f),L))) +
-    ggtitle(learner_id)
-}
+# draw_heatmap <- function(learner_id, legend_name=expression(OF(hat(f), L)), fill_col="of") {
+#   ggplot(df[df$learner_id==learner_id,], aes_string("dim", "sample", fill=fill_col)) +
+#     scale_x_continuous(trans = "log2", breaks = trans_breaks("log2", function(x) 2^x)) +
+#     scale_y_continuous(trans = "log2", breaks = trans_breaks("log2", function(x) 2^x)) +
+#     geom_tile() +
+#     scale_fill_viridis(end=0.9, name = legend_name) +
+#     ggtitle(learner_id)
+# }
+#
+# plots_of <- lapply(unique(df[["learner_id"]]), function(learner_id) draw_heatmap(learner_id))
+# plots_train_error <- lapply(unique(df[["learner_id"]]),
+#                             function(learner_id) draw_heatmap(learner_id, legend_name = "train error", fill_col = "train_error"))
+# plots_test_error <- lapply(unique(df[["learner_id"]]),
+#                             function(learner_id) draw_heatmap(learner_id, legend_name = "test error", fill_col = "test_error"))
+# p <- grid.arrange(grobs = c(plots_of, plots_train_error, plots_test_error), nrow = 3, ncol = 6)
+#
+# p
 
-plots <- lapply(unique(df[["learner_id"]]), draw_of_heatmap)
-p <- grid.arrange(grobs = plots, nrow = 1, ncol = 5)
+# ggsave(filename = "../figure/visualize_overfitting_plot_peak_boost_10rounds.png", plot=p, width=21, height=9)
 
-p
-
-ggsave(filename = "../figure/visualize_overfitting_plot_mlr.png", plot=p, width=18, height=3)
+tmp <- df[df$learner_id == "Linear Model",]
