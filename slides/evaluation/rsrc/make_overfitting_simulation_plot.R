a4d9f4510a2e3c1a591cd0e8d0b3796f3f0e17c1
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2021-12-13 11:21:26 +0100

    Made overfitting plot's font size larger + Updated tex script.

--- slides/evaluation/rsrc/make_overfitting_simulation_plot.R
+++ slides/evaluation/rsrc/make_overfitting_simulation_plot.R
@@ -9,8 +9,12 @@ df <- readRDS("overfitting_peak.rds")
 
 draw_heatmap <- function(learner_id, legend_name=expression(OF(hat(f), L)), fill_col="of") {
   ggplot(df[df$learner_id==learner_id,], aes_string("dim", "sample", fill=fill_col)) +
-    scale_x_continuous(trans = "log2", breaks = trans_breaks("log2", function(x) 2^x)) +
-    scale_y_continuous(trans = "log2", breaks = trans_breaks("log2", function(x) 2^x)) +
+    scale_x_continuous(trans = "log2",
+                       breaks = trans_breaks("log2", function(x) 2^x),
+                       labels = trans_format("log2", math_format(2^.x))) +
+    scale_y_continuous(trans = "log2",
+                       breaks = trans_breaks("log2", function(x) 2^x),
+                       labels = trans_format("log2", math_format(2^.x))) +
     geom_tile() +
     xlab("p") +
     ylab("n") +
@@ -25,5 +29,5 @@ plots_test_error <- lapply(unique(df[["learner_id"]])[1:5],
                             function(learner_id) draw_heatmap(learner_id, legend_name = expression(GE(hat(f), L)), fill_col = "test_error"))
 p <- grid.arrange(grobs = c(plots_of, plots_train_error, plots_test_error), nrow = 3, ncol = 5)
 
-ggsave(filename = "../figure/overfitting_simulation_plot.png", plot=p, width=18, height=9)
+ggsave(filename = "../figure/overfitting_simulation_plot.png", plot=p, width=14, height=6.5)
 
