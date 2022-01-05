060a27f8eda24eb5f90dc3939a3569ac962f0e6c
Xuan-Son Trinh <xuanson.trinh@outlook.com>
2022-01-05 10:11:49 +0100

    Added 3 new plots onto the tuning slides

--- slides/tuning/rsrc/tune_example.R
+++ slides/tuning/rsrc/tune_example.R
@@ -4,9 +4,6 @@ library(mlr3)
 library(mlr3learners)
 library(mlr3tuning)
 library(paradox)
-library(parallelMap)
-
-parallelStartMulticore(15)
 
 # DATA -------------------------------------------------------------------------
 
@@ -15,20 +12,20 @@ set.seed(123222)
 # Just run RS on spam for ranger so we can show an example path
 # that improves the result
 
-task = tsk("sonar")
-learner = lrn("classif.ranger", predict_type = "prob")
-resampling = rsmp("cv", folds = 5)
-measure = msr("classif.auc")
+task <- tsk("sonar")
+learner <- lrn("classif.ranger", predict_type = "prob")
+resampling <- rsmp("cv", folds = 5)
+measure <- msr("classif.auc")
 
-tune_ps = ParamSet$new(list(
+tune_ps <- ParamSet$new(list(
   ParamInt$new("num.trees", lower = 3, upper = 200),
   ParamInt$new("mtry", lower = 5, upper = 20),
   ParamInt$new("min.node.size", lower = 2, upper = 20)
 ))
 
-terminator = trm("evals", n_evals = 150)
+terminator <- trm("evals", n_evals = 216)
 
-instance = TuningInstanceSingleCrit$new(
+rs_instance <- TuningInstanceSingleCrit$new(
   task = task,
   learner = learner,
   resampling = resampling,
@@ -38,10 +35,23 @@ instance = TuningInstanceSingleCrit$new(
   store_models = TRUE
 )
 
-tuner = tnr("random_search")
-tuner$optimize(instance)
-tuning = instance$archive
+gs_instance <- TuningInstanceSingleCrit$new(
+  task = task,
+  learner = learner,
+  resampling = resampling,
+  measure = measure,
+  search_space = tune_ps,
+  terminator = terminator,
+  store_models = TRUE
+)
 
-BBmisc::save2("tune_example.RData", tune_example_res = tuning)
+rs_tuner <- tnr("random_search")
+rs_tuner$optimize(rs_instance)
+rs_tuning <- rs_instance$archive$data
 
-parallelStop()
+gs_tuner <- tnr("grid_search", resolution=6)
+gs_tuner$optimize(gs_instance)
+gs_tuning <- gs_instance$archive$data
+
+saveRDS(rs_tuning, "tune_rs_example.rds")
+saveRDS(gs_tuning, "tune_gs_example.rds")
