11ecefa17780798ba71f61f3e317c793f0cb9bd6
holgstr <64039523+holgstr@users.noreply.github.com>
2022-06-23 15:28:27 +0200

    creates figure for a kNN model with AUC = 0 (#802)
    * creates figure for a kNN model with AUC = 0
    
    * adds truth label to legend
    
    * try workaround
    
    * add mlr3learners

---
+++ slides/evaluation/rsrc/fig-eval_bad_model_auc.R
@@ -0,0 +1,36 @@
+# PREREQ -----------------------------------------------------------------------
+
+library(ggplot2)
+library(mlr3)
+library(mlr3viz)
+library(mlr3learners)
+
+# DATA -------------------------------------------------------------------------
+
+is.even = function(x) if ((x[1] + x[2]) %% 2 == 0) {0} else {1}
+df = expand.grid(1:10, 1:10)
+names(df) = c("x1", "x2")
+y = as.factor(apply(X=df, MARGIN = 1, FUN = is.even))
+df = data.frame(df, y)
+
+# MODEL -----------------------------------------------------------------------
+
+task = as_task_classif(df, id = "chess", target = "y")
+learner = lrn("classif.kknn", k=3, kernel = "rectangular", distance = 2, predict_type = "response")
+
+# PLOT -------------------------------------------------------------------------
+
+p = plot_learner_prediction(learner, task)
+p = p + scale_fill_viridis_d(end = .9, name = "Prediction") + guides(shape = FALSE, alpha = FALSE) + 
+    geom_point(aes(color = y), size = 1.5) + scale_colour_viridis_d(end = .9, name = "Truth")
+p
+
+ggsave("../figure/eval_bad_model_auc.pdf", p, width = 8L, height = 4L)
+
+# AUC -------------------------------------------------------------------------
+
+learner = lrn("classif.kknn", k=3, kernel = "rectangular", distance = 2, predict_type = "prob")
+predictions = learner$train(task)$predict_newdata(df)
+auc = msr("classif.auc")$score(predictions)
+auc
+
