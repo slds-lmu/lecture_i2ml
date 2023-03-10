de6cc85e724efe8a806d2eb40d42ad6e63b9144a
Lisa Wimmer <wml1293@outlook.de>
2023-03-10 10:48:01 +0100

    work on l2 chunk

--- slides/supervised-regression/rsrc/make_figs_l1_l2.R
+++ slides/supervised-regression/rsrc/make_figs_l1_l2.R
@@ -20,18 +20,17 @@ computer$computeRegression("l2_univ", y ~ x_1, loss = "quadratic")
 
 # BASIC UNIVARIATE PLOT --------------------------------------------------------
 
-plot_data <- do.call(rbind, computer$regression_data)
-plotter_2d <- RegressionPlotter$new(plot_data)
-plotter_2d$initLayer2D(y ~ x_1)
-plotter_2d$addScatter(alpha = 0.4)
-plotter_2d$addPredictionHyperplane(
+plot_univariate <- RegressionPlotter$new(
+    do.call(rbind, computer$regression_data)
+)
+plot_univariate$initLayer2D(y ~ x_1)
+plot_univariate$addScatter()
+plot_univariate$addPredictionHyperplane(
     "l2", computer$coefficients[[1]], col = "blue"
 )
-
-plot_univariate <- plotter_2d$plot()
 ggsave(
     "../figure/reg_lm_plot.pdf", 
-    plot_univariate, 
+    plot_univariate$plot(), 
     width = 2.5, 
     height = 2
 )
@@ -40,54 +39,49 @@ ggsave(
 
 theta_0 <- computer$coefficients[["l2_univ"]][1]
 theta_1 <- computer$coefficients[["l2_univ"]][2]
+my_lm <- function(x, coefs = c(theta_0, theta_1)) coefs[0] + coefs[1] * x
 summary(lm(y ~ x_1, dt_univ)) # save screenshot to figure_man
 
-plot_univariate_interpreted <- plot_univariate +
+plot_interpreted <- RegressionPlotter$new(
+    do.call(rbind, computer$regression_data)
+)
+plot_interpreted$initLayer2D(y ~ x_1)
+plot_interpreted$addScatter(alpha = 0.4, col = "darkgray")
+plot_interpreted$addPredictionHyperplane(
+    "l2", computer$coefficients[[1]], col = "black"
+)
+plot_interpreted <- plot_interpreted$plot() +
     geom_vline(xintercept = 0, color = "darkgray", linetype = "dotted") +
     geom_segment(
         mapping = aes(x = 0, y = 0, xend = 0, yend = theta_0),
         color = "blue"
     ) +
     geom_segment(
-        mapping = aes(
-            x = 2, 
-            y = theta_0 + theta_1 * 1, 
-            xend = 2, 
-            yend = theta_0 + theta_1 * 2
-        ),
+        mapping = aes(x = 2, y = my_lm(1), xend = 2, yend = my_lm(2)),
         color = "blue"
     ) +
     geom_segment(
-        mapping = aes(
-            x = 1, 
-            y = theta_0 + theta_1 * 1, 
-            xend = 2, 
-            yend = theta_0 + theta_1 * 1
-        ),
+        mapping = aes(x = 1, y = my_lm(1), xend = 2, yend = my_lm(1))
     ) +
     geom_text(
         mapping = aes(x = 0.2, y = theta_1, label = "{theta[0]}"),
         color = "blue",
-        vjust = 2,
+        vjust = 1,
         parse = TRUE
     ) +
     geom_text(
-        mapping = aes(
-            x = 2.2, y = theta_0 + theta_1 * 2, label = "{theta[1]}"
-        ),
+        mapping = aes(x = 2.2, y = my_lm(2), label = "{theta[1]}"),
         color = "blue",
         vjust = 2,
         parse = TRUE
     ) +
     geom_text(
-        mapping = aes(
-            x = 1.5, y = theta_0 + theta_1 * 1, label = "1 unit"
-        ), 
-        vjust = 2,
+        mapping = aes(x = 1.5, y = my_lm(1), label = "1 unit"), 
+        vjust = 1,
     )
 ggsave(
     "../figure/reg_lm_plot_interpreted.pdf", 
-    plot_univariate_interpreted, 
+    plot_interpreted, 
     width = 2.5, 
     height = 2
 )
@@ -95,52 +89,46 @@ ggsave(
 # UNIVARIATE PLOT WITH QUADRATIC RESIDUAL --------------------------------------
 
 idx_highlight <- 33
-plotter_2d$addResiduals(
+plot_residual <- plot_univariate$clone()
+plot_residual$addResiduals(
     plot_data[id == "l2_univ", y_hat], 
     idx_highlight, 
-    quadratic = TRUE, 
-    col = "blue"
+    quadratic = TRUE
 )
-plot_univariate_res <- plotter_2d$plot() +
-    geom_hline(yintercept = mean(dt_univ$y)) +
+plot_residual <- plot_residual$plot() +
     geom_point(
-        dt_univ[idx_highlight, ], mapping = aes(x_1, y), col = "red", size = 2
-    )+
-    geom_text(
-        mapping = aes(x = -1, y = mean(y) + 0.5, label = "{bar(y)}"),
-        vjust = 1,
-        parse = TRUE
+        dt_univ[idx_highlight, ], 
+        mapping = aes(x_1, y), 
+        col = "blue", 
+        size = 2,
+        shape = "cross"
     )
-ggsave("../figure/reg_lm_sse.pdf", plot_univariate_res, width = 3, height = 2.6)
+ggsave("../figure/reg_lm_sse.pdf", plot_residual, width = 3, height = 2.6)
 
 # LOSS PLOT --------------------------------------------------------------------
 
-idx_highlight <- c(1, 45)
-plotter_2d <- RegressionPlotter$new(plot_data)
-plotter_2d$initLayer2D(y ~ x_1)
-plotter_2d$addScatter(alpha = 0.4)
-plotter_2d$addPredictionHyperplane(
-    "l2", computer$coefficients[[1]], col = "blue"
-)
-plotter_2d$addResiduals(
-    plot_data[id == "l2_univ", y_hat], 
-    idx_highlight, 
-    quadratic = TRUE, 
-    col = "blue"
-)
-p_1 <- plotter_2d$plot()
-
+idx_highlight <- c(19, 49)
 residuals_highlight <- computer$regression_data[[1]]$residual[idx_highlight]
-plotter_loss <- LossPlotter$new(seq(-3, 3, by = 1))
-plotter_loss$initLayer()
-plotter_loss$addLossCurve("l2", function(x) x**2, col = "blue")
-plotter_loss$addAnnotation(function(x) x**2, residuals_highlight, linetype = 2)
-plotter_loss$addAnnotation(function(x) x**2, residuals_highlight, type = "point")
-plotter_loss$addAnnotation(function(x) x**2, residuals_highlight, type = "text")
-p_2 <- plotter_loss$plot()
-p <- grid.arrange(p_1, p_2, ncol = 2)
+loss_fun <- function(x) x**2
 
-ggsave("../figure/l2.pdf", p, width = 6, height = 2.5)
+plot_residual_2 <- plot_univariate$clone()
+plot_residual_2$addResiduals(
+    plot_data[id == "l2_univ", y_hat], idx_highlight, quadratic = TRUE
+)
+plot_loss <- LossPlotter$new(seq(-1.5, 1.5, by = 1))
+plot_loss$initLayer()
+plot_loss$addLossCurve("l2", loss_fun, col = "black")
+plot_loss$addAnnotation(loss_fun, residuals_highlight, linetype = 2)
+plot_loss$addAnnotation(loss_fun, residuals_highlight, type = "point")
+plot_loss$addAnnotation(
+    loss_fun, residuals_highlight, type = "text", nudge_x = 0.6
+)
+ggsave(
+    "../figure/l2.pdf", 
+    grid.arrange(plot_residual_2$plot(), plot_loss$plot(), ncol = 2), 
+    width = 6, 
+    height = 2.4
+)
 
 # BIVARIATE PLOT ---------------------------------------------------------------
 
@@ -151,5 +139,5 @@ plotter_3d <- RegressionPlotter$new(
 )
 plotter_3d$initLayer3D(y ~ x_1 + x_2)
 plotter_3d$addScatter(col = "black")
-plotter_3d$addPredictionHyperplane("l2", computer$coefficients[["l2_biv"]])
+plotter_3d$addPredictionHyperplane("l2", computer_biv$coefficients[["l2_biv"]])
 plotter_3d$plot()
