b9f3db2a2fb78e115c691cddcd10fe62b23ce55d
mb706 <mlr.developer@mb706.com>
2023-04-02 16:25:15 +0200

    adding margin for speaker

--- slides/tex.mk
+++ slides/tex.mk
@@ -18,25 +18,25 @@ copy:
 	cp *.pdf ../../slides-pdf
 	
 texclean: 
-	rm -rf *.out
-	rm -rf *.dvi
-	rm -rf *.log
-	rm -rf *.aux
-	rm -rf *.bbl
-	rm -rf *.blg
-	rm -rf *.ind
-	rm -rf *.idx
-	rm -rf *.ilg
-	rm -rf *.lof
-	rm -rf *.lot
-	rm -rf *.toc
-	rm -rf *.nav
-	rm -rf *.snm
-	rm -rf *.vrb
-	rm -rf *.fls
-	rm -rf *.fdb_latexmk
-	rm -rf *.synctex.gz
-	rm -rf *-concordance.tex
+	-rm -rf *.out
+	-rm -rf *.dvi
+	-rm -rf *.log
+	-rm -rf *.aux
+	-rm -rf *.bbl
+	-rm -rf *.blg
+	-rm -rf *.ind
+	-rm -rf *.idx
+	-rm -rf *.ilg
+	-rm -rf *.lof
+	-rm -rf *.lot
+	-rm -rf *.toc
+	-rm -rf *.nav
+	-rm -rf *.snm
+	-rm -rf *.vrb
+	-rm -rf *.fls
+	-rm -rf *.fdb_latexmk
+	-rm -rf *.synctex.gz
+	-rm -rf *-concordance.tex
 	
 clean: texclean
 	-rm $(TPDFS) 2>/dev/null || true
