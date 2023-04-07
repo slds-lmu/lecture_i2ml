b63b6559e1c2cd70b448b5ac8cf205f0edc5be17
AmelieLoessl <amelie.loessl@campus.lmu.de>
2023-04-07 11:54:25 +0200

    Code update after feedback

--- slides/supervised-regression/rsrc/poly_reg_example.R
+++ slides/supervised-regression/rsrc/poly_reg_example.R
@@ -14,7 +14,6 @@ library(GGally)
 library(cvTools)
 library(data.table)
 
-
 # load bike data set from OpenML
 bike_openML <- getOMLDataSet(data.id = 45103)
 
@@ -28,15 +27,8 @@ summary(bike_data)
 bike_data$rentals <- as.numeric(bike_data$rentals)
 # transformation of variable "mnth" into factor variable
 bike_data$mnth <- as.factor(bike_data$mnth)
-
-
-# scatter plot rentals on temp
-ggplot(data = bike_data, aes(x = temp, y = rentals)) + geom_point() +
-  theme_bw()
-
-# scatter plot rentals on mnth
-ggplot(data = bike_data, aes(x = mnth, y = rentals)) + geom_point() +
-  theme_bw()
+# transformation of variable "season" into factor variable
+bike_data$season <- as.factor(bike_data$season)
 
 
 # Code for CV
@@ -44,24 +36,22 @@ rmse <- function(observed, predicted) {
   return(sqrt(sum((observed - predicted)^2) / length(observed)))
 }
 
-
 get_folds <- function(n, K = n_folds) {
   cv_folds <- cvTools::cvFolds(n = n, K = K)
   cv_folds <- split(x = cv_folds$subsets[, 1], f = cv_folds$which)
   return(cv_folds)
 }
 
-
 do_cv <- function(data, folds, degree0) {
-  # Berechnung des Mittelwertes der RMSEs aller Teildatens¨atze:
+  # computes RMSE mean of all folds. 
   cv_mod_test <- mean(vapply(X = folds, FUN = function(i) {
-    model <- lm(rentals ~ poly(temp, degree = degree0) , data = data[-i, ])
+    model <- lm(rentals ~ poly(temp, degree = degree0) + season  + hum, data = data[-i, ])
     rmse(observed = data[i, "rentals"],
          predicted = predict(object = model, newdata = data[i,]))
   }, FUN.VALUE = numeric(1)))
   
   cv_mod_train <- mean(vapply(X = folds, FUN = function(i) {
-    model <- lm(rentals ~ poly(temp, degree = degree0) , data = data[-i, ])
+    model <- lm(rentals ~ poly(temp, degree = degree0) + season + hum, data = data[-i, ])
     rmse(observed = data[-i, "rentals"],
          predicted = predict(object = model, newdata = data[-i,]))
   }, FUN.VALUE = numeric(1)))
@@ -72,41 +62,29 @@ do_cv <- function(data, folds, degree0) {
   # generate effect plots:
   plot <- effect_plot(mod_data, pred = temp, data = data, plot.points = TRUE, 
               interval = TRUE, line.color = "blue", y.label = "number of rentals",
-              x.lab  = "normalized temperature", main.title = paste0("effect plot | degree: ", degree0))
-  
-  
+              x.lab  = "normalized temperature", main.title = paste0("effect plot degree ", degree0))
   
   # save coefficient estimates:
   estimated_effects <- mod_data$coefficients
   
-  
-  # Ausgabe des Kreuzvalidierungs-RMSEs:
   attr(x = cv_mod_test, which = "degree") <- degree0
   attr(x = cv_mod_train, which = "degree") <- degree0
   attr(x = cv_mod_test, which = "data") <- "test set: predicted GE"
   attr(x = cv_mod_train, which = "data") <- "train set: training error"
-  return(list(cv_mod_test, cv_mod_train, estimated_effects, plot))
+  return(list("cv_test_error" = cv_mod_test,"cv_train_error" = cv_mod_train, 
+              "coeff_estimates" =  estimated_effects, "plots" = plot))
 }
-
-
 
 do_cvs <- function(data, folds, degrees) {
-  # Generierung aller m¨oglichen Modellspezifikationen:
-  
-  # Durchf¨uhrung der Kreuzvalidierung f¨ur die einzelnen Modellspezifikationen:
+  # generate all possible specifications. 
+  # computes cv for each specification. 
   res_all <- lapply(degrees, FUN = function(z) do_cv(data = data, folds = folds, degree0 = z))
-  # Extraktion des gem¨aß des RMSEs besten Modells:
-  # model_best <- lm(formula = attr(x = get_best(res_all), which = "formula"),
-  #                  data = data)
-  # return(coef(model_best))
   return(res_all)
-  
 }
-
 
 # run CV + plots + coefficient estimates
 # set highest degree for polynomials
-degrees0 <- seq_len(8)
+degrees0 <- seq_len(10)
 # set number of folds for CV
 n_folds <- 10
 # run CV
@@ -116,33 +94,28 @@ set.seed(1)
 CV_results <- do_cvs(bike_data, folds = folds0, degrees = degrees0)
 
 # extract results for train and test error
-error_list <- lapply(CV_results, function(i) i[c(1,2)])
+error_list <- lapply(CV_results, function(i) i[c("cv_test_error","cv_train_error")])
 error_dt <- rbindlist(error_list)
 error_dt <- cbind(degrees0, error_dt)
 colnames(error_dt) <- c("degree", "test_error", "train_error")
 
 # plotting test and training error
-jpeg(file= "plot: train vs test error.jpeg")
-plot(x = error_dt$degree, y = error_dt$test_error, type = "l", ylim = c(122,130), 
-     ylab = "rmse", xlab = "degree", main = "test & training error")
-lines(x = error_dt$degree, y = error_dt$train_error, col = "red")
-legend(x = "topright",          # Position
-       legend = c("test", "train"),  # Legend texts
-       lty = c(1, 2),           # Line types
-       col = c("black", "red"),           # Line colors
-       lwd = 2)  
-dev.off()
+ggplot() + geom_line(data = error_dt, aes(x = degree, y = test_error, color = "test error")) + 
+  geom_line(data = error_dt, aes(x = degree, y = train_error, color = "train error")) + 
+  xlab("Degree") + ylab("rmse") + theme_bw() + 
+  labs(color='') 
+ggsave("figure/ploynomial_regression_test_train_error.png")
 
 # extract results for coefficient estimates
-coeff_list <- lapply(CV_results, function(i) i[[3]])
+coeff_list <- lapply(CV_results, function(i) i[["coeff_estimates"]])
 
 # extract plots
-plot_list <- lapply(CV_results, function(i) i[[4]])
+plot_list <- lapply(CV_results, function(i) i[["plots"]])
 
 # save plots
 for(i in degrees0){
   plot <- plot_list[[i]]
-  jpeg(file= paste0("effect_plot_degree_", i, ".jpeg"))
+  jpeg(file= paste0("figure/polynomial_regr_effect_plot_degree_", i, ".png"))
   print(plot)
   dev.off()
 }
