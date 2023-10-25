1b89724d2bf13a9134e6dda3e6bc38ef426639bb
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2023-10-25 16:13:42 +0200

    Updates from Overleaf

--- slides/tex.mk
+++ slides/tex.mk
@@ -1,34 +1,44 @@
+# Slide .tex files, relative paths
 TSLIDES = $(shell find . -maxdepth 1 -iname "slides-*.tex")
+# Substitute file extension tex -> pdf for output pdf filenames
 TPDFS = $(TSLIDES:%.tex=%.pdf)
-MARGINPDFS = $(TSLIDES:%.tex=%-margin.pdf)
+# output pdf filenames for slides without margin (old 4:3 layout)
+NOMARGINPDFS = $(TSLIDES:%.tex=%-nomargin.pdf)
+
 FLSFILES = $(TSLIDES:%.tex=%.fls)
 
-.PHONY: all most all-margin copy texclean clean
+.PHONY: all most all-nomargin copy texclean clean
 
-all: $(TPDFS) 
+# Default action compiles without margin and copies to slides-pdf!
+all: $(TPDFS)
 	$(MAKE) copy
 
-all-margin: $(MARGINPDFS)
+# derivative action does the same for slides without margin (different filenames!)
+all-nomargin: $(NOMARGINPDFS)
 	$(MAKE) copy
 
-most: $(FLSFILES) 
+# Analogously the same but without copying (arguably should be the default actions)
+most: $(FLSFILES)
+most-nomargin: $(NOMARGINPDFS)
 
+# Conditionally remove or create empty nospeakermargin.tex file to decide which layout to use
+# See /style/lmu-lecture.sty -- it's a whole thing but does the job.
 $(TPDFS): %.pdf: %.tex
-	-rm speakermargin.tex
+	-rm nospeakermargin.tex
 	latexmk -pdf $<
 
-$(MARGINPDFS): %-margin.pdf: %.tex
-	touch speakermargin.tex
-	latexmk -pdf -jobname=%A-margin $<
+$(NOMARGINPDFS): %-nomargin.pdf: %.tex
+	touch nospeakermargin.tex
+	latexmk -pdf -jobname=%A-nomargin $<
 
 $(FLSFILES): %.fls: %.tex
-	-rm speakermargin.tex
+	-rm nospeakermargin.tex
 	latexmk -pdf -g $<
 
 copy:
 	cp *.pdf ../../slides-pdf
-	
-texclean: 
+
+texclean:
 	-rm -rf *.out
 	-rm -rf *.dvi
 	-rm -rf *.log
@@ -48,7 +58,7 @@ texclean:
 	-rm -rf *.fdb_latexmk
 	-rm -rf *.synctex.gz
 	-rm -rf *-concordance.tex
-	-rm -rf speakermargin.tex
-	
+	-rm -rf nospeakermargin.tex
+
 clean: texclean
-	-rm $(TPDFS) $(MARGINPDFS) 2>/dev/null
+	-rm $(TPDFS) $(NOMARGINPDFS) 2>/dev/null
