7d04caa7750b94495709f1dc916757c1296af97f
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2024-09-27 14:42:11 +0200

    Updates from Overleaf

--- slides/forests/rsrc/fig-cart_forest_fimp.R
+++ slides/forests/rsrc/fig-cart_forest_fimp.R
@@ -1,60 +1,23 @@
-# goal here is to visualize typical results of impurity and permutation importance
-# using mtcars (to allow interpretation), and show how similar the results are.
+# PREREQ -----------------------------------------------------------------------
 
 library(knitr)
 library(ggplot2)
-library(mlr3)
-library(mlr3learners)
-library(mlr3viz)
-library(reshape2)
-library(ggplot2)
+library(randomForest)
 
 options(digits = 3, 
         width = 65, 
         str = strOptions(strict.width = "cut", vec.len = 3))
 
-task = tsk("mtcars")
+# DATA -------------------------------------------------------------------------
 
-# permutation importance for all features
-learner_perm = lrn("regr.ranger", importance = "permutation")
-learner_perm$train(task)
-importance_perm = learner_perm$importance()
+set.seed(600000)
+model = randomForest(Species ~ ., data = iris, importance = TRUE)
 
-# impurity importance for all features
-learner_imp = lrn("regr.ranger", importance = "impurity")
-learner_imp$train(task)
-importance_imp = learner_imp$model$variable.importance
+# PLOT -------------------------------------------------------------------------
 
-# convert importances to data frames for plotting
-importance_perm_df = data.frame(Feature = names(importance_perm), Importance = importance_perm)
-importance_imp_df = data.frame(Feature = names(importance_imp), Importance = importance_imp)
+pdf("../figure/cart_forest_fimp_1.pdf", width = 8, height = 2.8)
 
-# plotting & saving permutation importance
-ggplot(importance_perm_df, aes(x = reorder(Feature, Importance), y = Importance)) +
-  geom_col(fill = "#66CDAA") +
-  coord_flip() +
-  labs(x = "features of task mtcars",
-       y = "permutation importance: increase of MSE") +
-  theme(
-    axis.title.x = element_text(size = 12),
-    axis.title.y = element_text(size = 12),
-    axis.text.x = element_text(size = 10),
-    axis.text.y = element_text(size = 12)
-  )
+randomForest::varImpPlot(model, main = "")
 
-ggsave("../figure/forest-fimp_perm.png", width = 4, height = 3)
-
-# plotting & saving impurity importance
-ggplot(importance_imp_df, aes(x = reorder(Feature, Importance), y = Importance)) +
-  geom_col(fill = "#4682B4") +
-  coord_flip() +
-  labs(x = "features of task mtcars",
-       y = "impurity importance: decrease in Gini impurity") +
-  theme(
-    axis.title.x = element_text(size = 12),
-    axis.title.y = element_text(size = 12),
-    axis.text.x = element_text(size = 10),
-    axis.text.y = element_text(size = 12)
-  )
-
-ggsave("../figure/forest-fimp_gini.png", width = 4, height = 3)
+ggsave("../figure/cart_forest_fimp_1.pdf", width = 8, height = 2.8)
+dev.off()
--- slides/tex.mk
+++ slides/tex.mk
@@ -1,39 +1,18 @@
 # Slide .tex files, relative paths
-TSLIDES = $(shell find . -maxdepth 1 -iname "slides*.tex")
+TSLIDES = $(shell find . -maxdepth 1 -iname "slides-*.tex")
 # Substitute file extension tex -> pdf for output pdf filenames
 TPDFS = $(TSLIDES:%.tex=%.pdf)
-# Substitute file extension tex -> pdf for output pdf filenames
-TPAXS = $(TSLIDES:%.tex=%.pax)
 # output pdf filenames for slides without margin (old 4:3 layout)
 NOMARGINPDFS = $(TSLIDES:%.tex=%-nomargin.pdf)
 
 FLSFILES = $(TSLIDES:%.tex=%.fls)
 
-.PHONY: all most all-nomargin most-nomargin copy texclean clean help pax
-
-help:
-	@echo "\n --- Rendering slides"
-	@echo "all                : Renders slides to PDF and runs texclean + copy (see below)"
-	@echo "most               : Renders slides to PDF, does not copy or clean"
-	@echo "all-normargin      : Same as all, but renders 4:3 slides with -nomargin.pdf suffix"
-	@echo "most-normargin     : Same as most, analogous to all-normagin"
-	@echo "\n --- Cleaning up"
-	@echo "texclean           : Deletes all LaTeX detrituts (.log, .aux, .nav, .synctex, ...)"
-	@echo "clean              : Runs texclean and deletes all rendered slides"
-	@echo "\n --- Copying to /slides-pdf/ (!! Linked from course website !!)"
-	@echo "copy               : Copies PDF files to /slides-pdf/"
-	@echo "slides-pdf         : Runs texclean, renders slides, copies to /slides-pdf/, and texclean again"
-	@echo "\n --- Utilities"
-	@echo "pax                : Runs pdfannotextractor.pl (pax) to store hyperlinks etc. in .pax files for later use"
+.PHONY: all most all-nomargin copy texclean clean
 
 # Default action compiles without margin and copies to slides-pdf!
 all: $(TPDFS)
-	@if [ -d "../../latex-math" ]; then\
-		$(MAKE) texclean;\
-		$(MAKE) copy;\
-	else\
-		echo "Cannot find 'latex-math' in root directory";\
-	fi
+	$(MAKE) copy
+
 # derivative action does the same for slides without margin (different filenames!)
 all-nomargin: $(NOMARGINPDFS)
 	$(MAKE) copy
@@ -42,49 +21,22 @@ all-nomargin: $(NOMARGINPDFS)
 most: $(FLSFILES)
 most-nomargin: $(NOMARGINPDFS)
 
-slides-pdf:
-	@if [ -d "../../latex-math" ]; then\
-		make texclean;\
-		make $(TPDFS);\
-		make copy;\
-		make texclean;\
-	else\
-		echo "Cannot find 'latex-math' in root directory";\
-	fi
-
 # Conditionally remove or create empty nospeakermargin.tex file to decide which layout to use
 # See /style/lmu-lecture.sty -- it's a whole thing but does the job.
 $(TPDFS): %.pdf: %.tex
 	-rm nospeakermargin.tex
-	@echo render $<;
-	latexmk -halt-on-error -pdf $<
+	latexmk -pdf $<
 
 $(NOMARGINPDFS): %-nomargin.pdf: %.tex
 	touch nospeakermargin.tex
-	latexmk -halt-on-error -pdf -jobname=%A-nomargin $<
+	latexmk -pdf -jobname=%A-nomargin $<
 
 $(FLSFILES): %.fls: %.tex
 	-rm nospeakermargin.tex
-	latexmk -halt-on-error -pdf -g $<
+	latexmk -pdf -g $<
 
 copy:
-	cp -u *.pdf ../../slides-pdf
-	cp -u *.pax ../../slides-pdf
-
-# Extract pdf annotations, i.e. hyperlinks, for later reinsertion
-# When combining multiple PDFs into one (for slides/all/)
-# https://ctan.org/tex-archive/macros/latex/contrib/pax?lang=en
-# Depending on installation linked script in PATH does not have file extension
-pax:
-	@if command -v pdfannotextractor.pl &> /dev/null; then\
-		echo "Found pdfannotextractor.pl";\
-		pdfannotextractor.pl *.pdf;\
-	elif command -v pdfannotextractor &> /dev/null; then\
-		echo "Found pdfannotextractor";\
-		pdfannotextractor *.pdf;\
-	else\
-		echo "Did not find pdfannotextractor, install 'pax' with tlmgr";\
-	fi
+	cp *.pdf ../../slides-pdf
 
 texclean:
 	-rm -rf *.out
@@ -103,14 +55,10 @@ texclean:
 	-rm -rf *.snm
 	-rm -rf *.vrb
 	-rm -rf *.fls
-	-rm -rf *.pax
-	-rm -rf *.bbl
-	-rm -rf *.bcf
-	-rm -rf *.run.xml
 	-rm -rf *.fdb_latexmk
 	-rm -rf *.synctex.gz
 	-rm -rf *-concordance.tex
 	-rm -rf nospeakermargin.tex
 
 clean: texclean
-	-rm $(TPDFS) $(NOMARGINPDFS) $(TPAXS) 2>/dev/null
+	-rm $(TPDFS) $(NOMARGINPDFS) 2>/dev/null
