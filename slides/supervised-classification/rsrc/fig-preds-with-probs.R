53f197d5e79f4a091f041a5dce5f2fed125f9987
manuel helmerichs <manuelhelmerichs@icloud.com>
2024-10-30 19:58:13 +0100

    updated motivation

--- slides/supervised-classification/rsrc/fig-preds-with-probs.R
+++ slides/supervised-classification/rsrc/fig-preds-with-probs.R
@@ -1,50 +1,69 @@
-# here we want to plot how some datapoints are predicted (x -> pi(x))
-# PREREQ -----------------------------------------------------------------------
+# here we want to plot how some datapoints are predicted (x -> pi(x)),
+# first by a naive linear models (to motivate the logistic regression)
+# then by a logistic regression model
 
-library(knitr)
 library(ggplot2)
+library(knitr)
 
-options(digits = 3, 
-        width = 65, 
-        str = strOptions(strict.width = "cut", vec.len = 3))
-
+# common settings
+set.seed(1234)
 plot_width <- 20
 plot_height <- 10
 plot_dpi <- 300
 line_size <- 5
 base_size <- 40
-point_size <- 10
-
+point_size <- 13
 
 # DATA -------------------------------------------------------------------------
+# Generate data for logistic and linear models
+n <- 20
+x <- runif(n, min = 0, max = 7)
+y <- x + rnorm(n) > 3.5
+df <- data.frame(x = x, y = as.factor(as.numeric(y)))
 
-set.seed(1234)
+x_seq <- seq(0, 7, by = 0.01)
+dfn <- data.frame(x = x_seq)
 
-n = 20
-x = runif(n, min = 0, max = 7)
-y = x + rnorm(n) > 3.5
-df = data.frame(x = x, y = y)
+# MODELS -----------------------------------------------------------------------
+# Fit a linear model to showcase the issue
+model_linear <- lm(as.numeric(y) - 1 ~ x, data = df)
+df$prob_linear <- predict(model_linear)
+dfn$prob_linear <- predict(model_linear, newdata = dfn)
 
-model = glm(y ~ x, family = binomial(link = "logit"), data = df)
+# a logistic model that fixes this issue
+model_logistic <- glm(y ~ x, family = binomial(link = "logit"), data = df)
+df$prob_logistic <- predict(model_logistic, type = "response")
+df$score_logistic <- predict(model_logistic)
+dfn$prob_logistic <- predict(model_logistic, newdata = dfn, type = "response")
+dfn$score_logistic <- predict(model_logistic, newdata = dfn)
 
-df$score = predict(model)
-df$prob = predict(model, type = "response")
-df$y = as.factor(as.numeric(df$y))
-x = seq(0, 7, by = 0.01)
+# PLOTS -------------------------------------------------------------------------
+# fitted linear model plot
+p_linear <- ggplot(df, aes(x = x)) +
+  geom_line(data = dfn, aes(y = prob_linear), color = "black", size = line_size) +
+  geom_point(aes(y = prob_linear, color = y), size = point_size) +
+  scale_color_manual(values = c("0" = "#E69F00", "1" = "#0072B2"), name = "y") +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red", size = line_size - 2) +
+  geom_hline(yintercept = 1, linetype = "dashed", color = "red", size = line_size - 2) +
+  xlab("x") + ylab(expression(pi(x))) +
+  theme_minimal(base_size = base_size) +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 40),
+    plot.margin = margin(20, 20, 20, 20)
+  ) +
+  annotate("text", x = 4, y = 1.1, label = "Probability > 1", color = "red", size = 16, fontface = "bold") +
+  annotate("text", x = 3.5, y = -0.1, label = "Probability < 0", color = "red", size = 16, fontface = "bold")
+p_linear
 
-dfn = data.frame(x = x)
-dfn$prob = predict(model, newdata = dfn, type = "response")
-dfn$score = predict(model, newdata = dfn)
-
-# PLOT -------------------------------------------------------------------------
-
-p = ggplot() + 
-  geom_line(data = dfn, aes(x = x, y = prob), size = line_size) +
-  geom_point(data = df, aes(x = x, y = prob, colour = y), size = point_size) +
+# logistic regression plot
+p_logistic <- ggplot() + 
+  geom_line(data = dfn, aes(x = x, y = prob_logistic), size = line_size, color = "black") +
+  geom_point(data = df, aes(x = x, y = prob_logistic, colour = y), size = point_size) +
   xlab("x") + ylab(expression(pi(x))) +
   scale_color_manual(values = c("#0072B2", "#E69F00")) +
   theme_minimal(base_size = base_size)
+p_logistic
 
-p
-
-ggsave("../figure/preds_with_probs.png", plot = p, width = plot_width, height = plot_height, dpi = plot_dpi)
+# save the plots
+ggsave("../figure/preds_with_probs-linear.png", plot = p_linear, width = plot_width, height = plot_height, dpi = plot_dpi)
+ggsave("../figure/preds_with_probs-logistic.png", plot = p_logistic, width = plot_width, height = plot_height, dpi = plot_dpi)
