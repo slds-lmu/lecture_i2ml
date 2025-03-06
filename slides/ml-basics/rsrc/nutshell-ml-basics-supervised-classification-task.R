65f04b75693b8d4cb943c03ed64d463e0b208818
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2025-03-06 10:23:43 +0100

    Updates from Overleaf

--- slides/ml-basics/rsrc/nutshell-ml-basics-supervised-classification-task.R
+++ slides/ml-basics/rsrc/nutshell-ml-basics-supervised-classification-task.R
@@ -1,6 +1,8 @@
 library(mlbench)
 library(kernlab)
-#
+
+set.seed(9)
+
 data = mlbench.twonorm(n = 50, d = 2)
 data = as.data.frame(data)
 X = as.matrix(data[, 1:2])
@@ -11,13 +13,20 @@ colnames(data) <- c("x.1", "x.2", "Y")
 mod_logreg = glm(Y ~ ., data = data, family = binomial())
 
 classif_plot <- ggplot(aes(x = x.1, y = x.2, group = Y), data = data) + 
-  geom_point(size = 6, aes(shape=Y, color = Y)) +
+  geom_point(size = 6, aes(shape = Y, color = Y)) +
   geom_abline(intercept = - coef(mod_logreg)[1] / coef(mod_logreg)[2],
-              slope = - coef(mod_logreg)[2] / coef(mod_logreg)[3], color = "black",
-              lwd = 1) + labs() +   xlab("x1") + ylab("x2") +
-  theme(axis.text=element_text(size=18),axis.title=element_text(size=30,face="bold"), 
-        legend.key.size = unit(2, 'cm'), legend.title = element_text(size=25, face = "bold"),
-        legend.text = element_text(size=18), legend.title.align=0.3)
+              slope = - coef(mod_logreg)[2] / coef(mod_logreg)[3],
+              color = "black", lwd = 1) +
+  labs() +  
+  xlab("x1") + ylab("x2") +
+  scale_color_discrete(labels = c("0", "1")) +
+  scale_shape_discrete(labels = c("0", "1")) +
+  theme(axis.text = element_text(size = 18),
+        axis.title = element_text(size = 30, face = "bold"), 
+        legend.key.size = unit(2, 'cm'),
+        legend.title = element_text(hjust = 0.3, size = 25, face = "bold"),
+        legend.text = element_text(size = 18))
+classif_plot
 
-ggsave(filename = "figure/nutshell-ml-basics-supervised-classification-task.png", 
+ggsave(filename = "../figure/nutshell-ml-basics-supervised-classification-task.png", 
        plot = classif_plot , width = 16, height = 11, units = "in")
